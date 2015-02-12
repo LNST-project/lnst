895d6c5aecab8ecc4df2685fbdfa5451c498abc5
Jan Tluka <jtluka@redhat.com>
2015-02-12 17:47:01 +0100

    regression-tests: extend test 25 to include the PktCounter test
    Signed-off-by: Jan Tluka <jtluka@redhat.com>
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>

--- regression-tests/tests/25/run.sh
+++ regression-tests/tests/25/run.sh
@@ -12,9 +12,14 @@ lnst-ctl -c lnst-ctl.conf -d run recipe2.xml | tee test.log
 rv2=${PIPESTATUS[0]}
 log2=`cat test.log`
 
+lnst-ctl -d run recipe3.xml | tee test.log
+rv3=${PIPESTATUS[0]}
+log3=`cat test.log`
+
 print_separator
 assert_status "pass" "$rv1"
 assert_status "pass" "$rv2"
+assert_status "pass" "$rv3"
 
 rm -f test.log
 
