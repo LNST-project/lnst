154da1d774e9ddaf3946ab5bfd3a92fd7a3f12d0
Jiri Pirko <jiri@resnulli.us>
2015-04-27 18:53:50 +0200

    8 release
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>

--- setup.py
+++ setup.py
@@ -171,7 +171,7 @@ DATA_FILES = CONFIG + TEST_MODULES + MULTICAST_TEST_TOOLS + MAN_PAGES + \
              SCHEMAS + BASH_COMP + RECIPE_FILES
 
 setup(name="lnst",
-    version="7",
+    version="8",
     description="Linux Network Stack Test",
     author="LNST Team",
     author_email="lnst-developers@lists.fedorahosted.org",
