742d4050d3f283018975c6d0437c7b4275d277fe
Jiri Prochazka <jprochaz@redhat.com>
2015-05-21 14:24:19 +0200

    Add Slave methods for NetEm value parsing from interface config
    This patch provides Slave methods for parsing values from NetEm
    tag in config dict.
    
    Signed-off-by: Jiri Prochazka <jprochaz@redhat.com>
    Signed-off-by: Jan Tluka <jtluka@redhat.com>
    Acked-by: Jiri Pirko <jiri@resnulli.us>

--- lnst/Slave/NetConfigCommon.py
+++ lnst/Slave/NetConfigCommon.py
@@ -35,3 +35,80 @@ def get_slave_option(netdev, slave_id, opt_name):
         if option == opt_name:
             return value
     return None
+
+def get_netem_option(netem_tag, netem_name, opt_name):
+    try:
+        options = netem_tag[netem_name]
+    except KeyError:
+        return None
+    for opt in options:
+        if opt["name"] == opt_name:
+            return opt["value"]
+
+def parse_delay(config):
+    time = get_netem_option(config, "delay", "time")
+    jitter= get_netem_option(config, "delay", "jitter")
+    correlation = get_netem_option(config, "delay", "correlation")
+    distribution = get_netem_option(config, "delay", "distribution")
+    rv = "delay %s " % time
+    if jitter is not None:
+        rv = rv + "%s " % jitter
+        if correlation is not None:
+            rv = rv + "%s " % correlation
+        if distribution is not None:
+                rv = rv + "distribution %s " % distribution
+    return rv
+
+def parse_loss(config):
+    percent = get_netem_option(config, "loss", "percent")
+    correlation = get_netem_option(config, "loss", "correlation")
+    rv = "loss %s " % percent
+    if correlation is not None:
+        rv = rv + "%s " % correlation
+    return rv
+
+def parse_corrupt(config):
+    percent = get_netem_option(config, "corrupt", "percent")
+    correlation = get_netem_option(config, "corrupt", "correlation")
+    rv = "corrupt %s " % percent
+    if correlation is not None:
+        rv = rv + "%s " % correlation
+    return rv
+
+def parse_duplication(config):
+    percent = get_netem_option(config, "duplication", "percent")
+    correlation = get_netem_option(config, "duplication", "correlation")
+    rv = "duplicate %s " % percent
+    if correlation is not None:
+        rv = rv + "%s " % correlation
+    return rv
+
+def parse_reordering(config):
+    percent = get_netem_option(config, "reordering", "percent")
+    correlation = get_netem_option(config, "reordering", "correlation")
+    gap_distance = get_netem_option(config, "reordering", "gap_distance")
+    rv = "reorder %s " % percent
+    if correlation is not None:
+        rv = rv + "%s " % correlation
+    if gap_distance is not None:
+        rv = rv + "gap %s " % gap_distance
+    return rv
+
+def parse_netem(config):
+    rv = ""
+    # delay parsing
+    if "delay" in config:
+        rv = rv + parse_delay(config)
+    # loss parsing
+    if "loss" in config:
+        rv = rv + parse_loss(config)
+    # corrupt
+    if "corrupt" in config:
+        rv = rv + parse_corrupt(config)
+    # duplication
+    if "duplication" in config:
+        rv = rv + parse_duplication(config)
+    # reordering
+    if "reordering" in config:
+        rv = rv + parse_reordering(config)
+    return rv
