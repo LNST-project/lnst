LNST-project/lnst

commit 92b8ae0dc8ac25a10835d9898172980b3af89ec1

Store interfaces driver info in SlavePool mapping and show it in summary
For issue #111. Since RPC connection is created after mapping, we can't get
interface driver info sooner without creating extra connection to the Slave.
This patch stores driver info after device configuration, after method
interface_update() was called, so driver info should be present in Slave.

Signed-off-by: Jiri Prochazka <jprochaz@redhat.com>
Signed-off-by: Jan Tluka <jtluka@redhat.com>
Acked-by: Ondrej Lichtner <olichtne@redhat.com>

lnst/Controller/NetTestController.py
@@ -204,6 +204,11 @@ class NetTestController:
 
             for iface in ifaces:
                 iface.configure()
+                if m._libvirt_domain is None:
+                    driver = iface._driver
+                    if_id = iface._id
+                    mapped_machine = self._slave_pool._map['machines'][m_id]
+                    mapped_machine['interfaces'][if_id]['driver'] = driver
             for iface in ifaces:
                 iface.up()
 

lnst/Controller/NetTestResultSerializer.py
@@ -100,9 +100,17 @@ class NetTestResultSerializer:
                                         (m_id, m["target"]), ""))
                     for if_id, pool_if in m["interfaces"].iteritems():
                         pool_id = pool_if["target"]
-                        output_pairs.append((6*" " + "interface \"%s\" "\
-                                                    "matched to \"%s\"" %\
-                                                    (if_id, pool_id), ""))
+                        if "driver" in pool_if:
+                            driver = pool_if["driver"]
+                            output_pairs.append((6*" " + "interface \"%s\" "
+                                                        "matched to \"%s\" "
+                                                        "(driver: \"%s\")" %
+                                                        (if_id, pool_id,
+                                                            driver), ""))
+                        else:
+                            output_pairs.append((6*" " + "interface \"%s\" "
+                                                        "matched to \"%s\" " %
+                                                        (if_id, pool_id), ""))
 
             if recipe["result"] == "FAIL" and \
                "err_msg" in recipe and recipe["err_msg"] != "":

commit 13e9337b0af0ef01e3caf91a9472aed8e35924f9

NetTestController: iterate only over StaticInterfaces to get driver info
This is a fix for corner case bug introduced by recent commit
92b8ae0dc8ac25a10835d9898172980b3af89ec1.

The problem is that if the match results with at least one
UnusedInterface we end up iterating over such interface that
unfortunately does not have self._id set since it defaults to
None.

The solution is to query driver information for StaticInterface
interface classes only.

Fixes #131

Signed-off-by: Jan Tluka <jtluka@redhat.com>
Acked-by: Ondrej Lichtner <olichtne@redhat.com>

lnst/Controller/NetTestController.py
@@ -27,6 +27,7 @@ from lnst.Common.NetTestCommand import str_command, CommandException
 from lnst.Controller.RecipeParser import RecipeParser, RecipeError
 from lnst.Controller.SlavePool import SlavePool
 from lnst.Controller.Machine import MachineError, VirtualInterface
+from lnst.Controller.Machine import StaticInterface
 from lnst.Common.ConnectionHandler import send_data, recv_data
 from lnst.Common.ConnectionHandler import ConnectionHandler
 from lnst.Common.Config import lnst_config
@@ -201,7 +202,8 @@ class NetTestController:
 
             for iface in ifaces:
                 iface.configure()
-                if m._libvirt_domain is None:
+                if (m._libvirt_domain is None and
+                        isinstance(iface, StaticInterface)):
                     driver = iface._driver
                     if_id = iface._id
                     mapped_machine = self._slave_pool._map['machines'][m_id]