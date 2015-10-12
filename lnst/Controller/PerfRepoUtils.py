21f29d8fdf3f221c821f59667ecb2feb6e3d786b
Ondrej Lichtner <olichtne@redhat.com>
2015-10-12 16:19:22 +0200

    PerfRepoUtils: remove mapping helper functions
    This functionality is now handled by the PerfRepoMapping class. Keeping
    the module as other functions will be added later.
    
    Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

--- lnst/Controller/PerfRepoUtils.py
+++ lnst/Controller/PerfRepoUtils.py
@@ -11,23 +11,3 @@ __author__ = """
 olichtne@redhat.com (Ondrej Lichtner)
 """
 
-import re
-
-def parse_id_mapping(filename):
-    line_re = re.compile(r"^(\w+)\s*=\s*(\w+)\s*$")
-    res_dict = {}
-    try:
-        with open(filename) as f:
-            for line in f:
-                match = line_re.match(line)
-                if match is not None:
-                    res_dict[match.group(1)] = match.group(2)
-    except:
-        return None
-    return res_dict
-
-def get_id(mapping, key):
-    try:
-        return mapping[key]
-    except (KeyError, TypeError):
-        return None
