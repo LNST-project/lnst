f713e1675dae2b6ace9b35fdac79e10d4134901d
Ondrej Lichtner <olichtne@redhat.com>
2015-10-12 16:19:19 +0200

    PerfRepo: add parameter log to REST API methods
    This is usefull when we call these methods to just check the result and
    don't want to add mess to the logs. For example if a user wants to get a
    test specified by ID or UID he first calls the get_by_id method that
    fails, but then the get_by_uid succeeds he probably doesn't want the
    first attempt to clutter up the logs with errors.
    
    Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

--- lnst/Controller/PerfRepo.py
+++ lnst/Controller/PerfRepo.py
@@ -839,166 +839,190 @@ class PerfRepoRESTAPI(object):
             return ""
         return self._url + obj.get_obj_url()
 
-    def test_get_by_id(self, test_id):
+    def test_get_by_id(self, test_id, log=True):
         get_url = self._url + '/rest/test/id/%s' % test_id
         response = self._session.get(get_url)
         if response.status_code != 200:
-            logging.debug(response.text)
+            if log:
+                logging.debug(response.text)
             return None
         else:
-            logging.info("GET %s success" % get_url)
+            if log:
+                logging.info("GET %s success" % get_url)
             return PerfRepoTest(response.content)
 
-    def test_get_by_uid(self, test_uid):
+    def test_get_by_uid(self, test_uid, log=True):
         get_url = self._url + '/rest/test/uid/%s' % test_uid
         response = self._session.get(get_url)
         if response.status_code != 200:
-            logging.debug(response.text)
+            if log:
+                logging.debug(response.text)
             return None
         else:
-            logging.info("GET %s success" % get_url)
+            if log:
+                logging.info("GET %s success" % get_url)
             return PerfRepoTest(response.content)
 
-    def test_create(self, test):
+    def test_create(self, test, log=True):
         post_url = self._url + '/rest/test/create'
         response = self._session.post(post_url, data=test.to_xml_string())
         if response.status_code != 201:
-            logging.debug(response.text)
+            if log:
+                logging.debug(response.text)
             return None
         else:
             new_id = response.headers["Location"].split('/')[-1]
             test.set_id(new_id)
-            logging.info("POST %s success" % post_url)
-            logging.info("Obj url: %s" % self.get_obj_url(test))
+            if log:
+                logging.info("POST %s success" % post_url)
+                logging.info("Obj url: %s" % self.get_obj_url(test))
             return test
 
-    def test_add_metric(self, test_id, metric):
+    def test_add_metric(self, test_id, metric, log=True):
         post_url = self._url + '/rest/test/id/%s/addMetric' % test_id
         response = self._session.post(post_url, data=metric.to_xml_string)
         if response.status_code != 201:
-            logging.debug(response.text)
+            if log:
+                logging.debug(response.text)
             return None
         else:
             new_id = response.headers["Location"].split('/')[-1]
             metric.set_id(new_id)
-            logging.info("POST %s success" % post_url)
-            logging.info("Obj url: %s" % self.get_obj_url(test))
+            if log:
+                logging.info("POST %s success" % post_url)
+                logging.info("Obj url: %s" % self.get_obj_url(test))
             return metric
 
-    def test_delete(self, test_id):
+    def test_delete(self, test_id, log=True):
         delete_url = self._url + '/rest/test/id/%s' % test_id
         response = self._session.delete(delete_url)
         if response.status_code != 204:
             return False
         else:
-            logging.info("DELETE %s success" % delete_url)
+            if log:
+                logging.info("DELETE %s success" % delete_url)
             return True
 
-    def metric_get(self, metric_id):
+    def metric_get(self, metric_id, log=True):
         get_url = self._url + '/rest/metric/%s' % metric_id
         response = self._session.get(get_url)
         if response.status_code != 200:
-            logging.debug(response.text)
+            if log:
+                logging.debug(response.text)
             return None
         else:
-            logging.info("GET %s success" % get_url)
+            if log:
+                logging.info("GET %s success" % get_url)
             return PerfRepoMetric(response.content)
 
-    def testExecution_get(self, testExec_id):
+    def testExecution_get(self, testExec_id, log=True):
         get_url = self._url + '/rest/testExecution/%s' % testExec_id
         response = self._session.get(get_url)
         if response.status_code != 200:
-            logging.debug(response.text)
+            if log:
+                logging.debug(response.text)
             return None
         else:
-            logging.info("GET %s success" % get_url)
+            if log:
+                logging.info("GET %s success" % get_url)
             return PerfRepoTestExecution(response.content)
 
-    def testExecution_create(self, testExec):
+    def testExecution_create(self, testExec, log=True):
         post_url = self._url + '/rest/testExecution/create'
         response = self._session.post(post_url, data=testExec.to_xml_string())
         if response.status_code != 201:
-            logging.debug(response.text)
+            if log:
+                logging.debug(response.text)
             return None
         else:
             new_id = response.headers["Location"].split('/')[-1]
             testExec.set_id(new_id)
-            logging.info("POST %s success" % post_url)
-            logging.info("Obj url: %s" % self.get_obj_url(testExec))
+            if log:
+                logging.info("POST %s success" % post_url)
+                logging.info("Obj url: %s" % self.get_obj_url(testExec))
             return testExec
 
-    def testExecution_delete(self, testExec_id):
+    def testExecution_delete(self, testExec_id, log=True):
         delete_url = self._url + '/rest/testExecution/%s' % testExec_id
         response = self._session.delete(delete_url)
         if response.status_code != 204:
-            logging.debug(response.text)
+            if log:
+                logging.debug(response.text)
             return False
         else:
-            logging.info("DELETE %s success" % delete_url)
+            if log:
+                logging.info("DELETE %s success" % delete_url)
             return True
 
-    def testExecution_add_value(self, value):
+    def testExecution_add_value(self, value, log=True):
         post_url = self._url + '/rest/testExecution/addValue'
         #TODO
         return self._session.post(post_url, data=value)
 
-    def testExecution_get_attachment(self, attachment_id):
+    def testExecution_get_attachment(self, attachment_id, log=True):
         get_url = self._url + '/rest/testExecution/attachment/%s' % \
                                                                 attachment_id
         #TODO
         return self._session.get(get_url)
 
-    def testExecution_add_attachment(self, testExec_id, attachment):
+    def testExecution_add_attachment(self, testExec_id, attachment, log=True):
         post_url = self._url + '/rest/testExecution/%s/addAttachment' % \
                                                                 testExec_id
         #TODO
         return self._session.post(post_url, data=attachment)
 
-    def report_get_by_id(self, report_id):
+    def report_get_by_id(self, report_id, log=True):
         get_url = self._url + '/rest/report/id/%s' % report_id
         response = self._session.get(get_url)
         if response.status_code != 200:
-            logging.debug(response.text)
+            if log:
+                logging.debug(response.text)
             return None
         else:
-            logging.info("GET %s success" % get_url)
+            if log:
+                logging.info("GET %s success" % get_url)
             return PerfRepoReport(response.content)
 
-    def report_create(self, report):
+    def report_create(self, report, log=True):
         post_url = self._url + '/rest/report/create'
 
         report.set_user(self._user)
 
         response = self._session.post(post_url, data=report.to_xml_string())
         if response.status_code != 201:
-            logging.debug(response.text)
+            if log:
+                logging.debug(response.text)
             return None
         else:
             new_id = response.headers["Location"].split('/')[-1]
             report.set_id(new_id)
-            logging.info("POST %s success" % post_url)
-            logging.info("Obj url: %s" % self.get_obj_url(report))
+            if log:
+                logging.info("POST %s success" % post_url)
+                logging.info("Obj url: %s" % self.get_obj_url(report))
             return report
 
-    def report_update(self, report):
+    def report_update(self, report, log=True):
         post_url = self._url + '/rest/report/update/%s' % report.get_id()
 
         report.set_user(self._user)
 
         response = self._session.post(post_url, data=report.to_xml_string())
         if response.status_code != 201:
-            logging.debug(response.text)
+            if log:
+                logging.debug(response.text)
             return None
         else:
-            logging.info("UPDATE %s success" % post_url)
-            logging.info("Obj url: %s" % self.get_obj_url(report))
+            if log:
+                logging.info("UPDATE %s success" % post_url)
+                logging.info("Obj url: %s" % self.get_obj_url(report))
             return report
 
-    def report_delete_by_id(self, report_id):
+    def report_delete_by_id(self, report_id, log=True):
         delete_url = self._url + '/rest/report/id/%s' % report_id
         response = self._session.delete(delete_url)
         if response.status_code != 204:
             return False
         else:
-            logging.info("DELETE %s success" % delete_url)
+            if log:
+                logging.info("DELETE %s success" % delete_url)
             return True
