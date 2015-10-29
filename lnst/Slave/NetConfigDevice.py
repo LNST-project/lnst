LNST-project/lnst

commit 83e442284c047e5defc78686d89064b3b45b27f4

NetConfigDevice: don't set loopback device down
We do not want to bring loopback device down in root namespace as
this might have an unpredictable impact on further testing.
In case of non-root namespace leaving loopback device up is not
a problem since the namespace will get destroyed after recipe is
finished anyway.

Signed-off-by: Jan Tluka <jtluka@redhat.com>
Acked-by: Jiri Prochazka <jprochaz@redhat.com>
Acked-by: Ondrej Lichtner <olichtne@redhat.com>

lnst/Slave/NetConfigDevice.py
@@ -97,6 +97,14 @@ class NetConfigDeviceLoopback(NetConfigDeviceGeneric):
     def configure(self):
         config = self._dev_config
 
+    def down(self):
+        # We do not want to bring loopback device down in root namespace as
+        # this might have an unpredictable impact on further testing.
+        # In case of non-root namespace leaving loopback device up is not
+        # a problem since the namespace will get destroyed after recipe is
+        # finished
+        pass
+
 class NetConfigDeviceBond(NetConfigDeviceGeneric):
     _modulename = "bonding"
     _moduleparams = "max_bonds=0"

commit dca45e4e0508244de214d93b4cffd6f61d5b9e69

NetConfigDevice: fix regression in Loopback device
This is the fix for regression introduced by

commit 83e442284c047e5defc78686d89064b3b45b27f4
NetConfigDevice: don't set loopback device down

NetconfigDeviceGeneric.down() method does also address deconfiguration
and the commit caused that loopback devices kept the configured
addresses after cleanup. The solution is to override the down() method
and do the address removal only.

Signed-off-by: Jan Tluka <jtluka@redhat.com>

lnst/Slave/NetConfigDevice.py
@@ -102,8 +102,12 @@ class NetConfigDeviceLoopback(NetConfigDeviceGeneric):
         # this might have an unpredictable impact on further testing.
         # In case of non-root namespace leaving loopback device up is not
         # a problem since the namespace will get destroyed after recipe is
-        # finished
-        pass
+        # finished. So, we will remove the configured addresses only
+        config = self._dev_config
+        if "addresses" in config:
+            for address in config["addresses"]:
+                exec_cmd("ip addr del %s dev %s" % (address, config["name"]),
+                         die_on_err=False)
 
 class NetConfigDeviceBond(NetConfigDeviceGeneric):
     _modulename = "bonding"