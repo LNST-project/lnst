LNST-project/lnst

commit 23aca15b17e7345854f55c19694a43b67cbc76a2

NetTestResultSerializer: include description of command in summary
User specified description is printed in log only. It's more useful to
include this also in summary report.

The output will be similar to this:

INFO:          host 1: exec     cmd: "sleep 1"                             PASS
INFO:              description: short sleep to give some time to settle

Signed-off-by: Jan Tluka <jtluka@redhat.com>
Signed-off-by: Jiri Pirko <jiri@mellanox.com>

lnst/Controller/NetTestResultSerializer.py
@@ -156,6 +156,9 @@ class NetTestResultSerializer:
 
         output_pairs.append((8*" " + m_id + cmd_res["res_header"], res))
 
+        if "desc" in command:
+            output_pairs.append((12*" " + "description: " + command["desc"], ""))
+
         if "msg" in cmd_res and cmd_res["msg"] != "":
             output_pairs.append((12*" " + "message: " + cmd_res["msg"], ""))
 

commit b2de883ee7d84390fc647eeb9c4ae18414127dfc

NetTestResultSerializer: check if desc is None
Fixes: 23aca15b1 ("NetTestResultSerializer: include description of command in summary")
Signed-off-by: Jiri Pirko <jiri@mellanox.com>

lnst/Controller/NetTestResultSerializer.py
@@ -156,7 +156,7 @@ class NetTestResultSerializer:
 
         output_pairs.append((8*" " + m_id + cmd_res["res_header"], res))
 
-        if "desc" in command:
+        if "desc" in command and command["desc"] != None:
             output_pairs.append((12*" " + "description: " + command["desc"], ""))
 
         if "msg" in cmd_res and cmd_res["msg"] != "":