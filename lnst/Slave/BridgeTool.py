LNST-project/lnst

commit 7a9a8585c6f42475f9ecd27c1bd4d0bb98e2e4c0

Task: add interface to work with bridge FDB entries
Kernel exposes an interface primary used by bridge to work with FDB entries.
However, other devices (for example SR-IOV drivers) implement this as well
making this not bridge-specific.

Allow task to query bridge FDB entries. This is implemented
simply by calling "bridge" utility. This may be later on changed to
pyroute2 implementation, alas it does not implement PF_BRIDGE Netlink
at the moment.

Signed-off-by: Jiri Pirko <jiri@mellanox.com>

lnst/Slave/BridgeTool.py
@@ -68,3 +68,21 @@ class BridgeTool:
 
     def del_fdb(self, br_fdb_info):
         return self._add_del_fdb("del", br_fdb_info)
+
+    def get_fdbs(_self):
+        output = exec_cmd("bridge fdb show dev %s" % _self._dev_name,
+                          die_on_err=False)[0]
+        br_fdb_info_list = []
+        for line in output.split("\n"):
+            match = re.match(r'([0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2})', line)
+            if match:
+                hwaddr = match.groups()[0]
+                match = re.match(r'.*\s+vlan (\d+)', line)
+                vlan_id = match.groups()[0] if match else 0
+                self = True if re.match(r'.*\s+self', line) else False
+                master = True if re.match(r'.*\s+master', line) else False
+                offload = True if re.match(r'.*\s+offload', line) else False
+                br_fdb_info = {"hwaddr": hwaddr, "vlan_id": vlan_id,
+                               "self": self, "master": master, "offload": offload}
+                br_fdb_info_list.append(br_fdb_info)
+        return br_fdb_info_list

lnst/Controller/Machine.py
@@ -782,6 +782,9 @@ class Interface(object):
         self._machine._rpc_call_x(self._netns, "del_br_fdb",
                                   self._id, br_fdb_info)
 
+    def get_br_fdbs(self):
+        return self._machine._rpc_call_x(self._netns, "get_br_fdbs", self._id)
+
     def set_speed(self, speed):
         self._machine._rpc_call_x(self._netns, "set_speed", self._id, speed)
 

lnst/Controller/Task.py
@@ -557,6 +557,9 @@ class InterfaceAPI(object):
         _self._if.del_br_fdb({"hwaddr": hwaddr, "self": self, "master": master,
                               "vlan_id": vlan_tci})
 
+    def get_br_fdbs(self):
+        return self._if.get_br_fdbs()
+
     def set_speed(self, speed):
         return self._if.set_speed(speed)
 

lnst/Slave/NetTestSlave.py
@@ -737,6 +737,14 @@ class SlaveMethods:
         brt.del_fdb(br_fdb_info)
         return True
 
+    def get_br_fdbs(self, if_id):
+        dev = self._if_manager.get_mapped_device(if_id)
+        if not dev:
+            logging.error("Device with id '%s' not found." % if_id)
+            return False
+        brt = BridgeTool(dev.get_name())
+        return brt.get_fdbs()
+
     def set_speed(self, if_id, speed):
         dev = self._if_manager.get_mapped_device(if_id)
         if dev is not None:

commit ddfa9d039c91d97707f35b43225645c8d2442c33

Task: Normalize hardware address when dumping FDB
Allows for easy comparison, as this is done throughout the code.

Fixes: 7a9a8585c6f4 ("Task: add interface to work with bridge FDB
entries")
Signed-off-by: Ido Schimmel <idosch@mellanox.com>
Signed-off-by: Jiri Pirko <jiri@mellanox.com>

lnst/Slave/BridgeTool.py
@@ -10,6 +10,7 @@ __author__ = """
 jiri@mellanox.com (Jiri Pirko)
 """
 
+from lnst.Common.NetUtils import normalize_hwaddr
 from lnst.Common.ExecCmd import exec_cmd
 import re
 
@@ -76,7 +77,7 @@ class BridgeTool:
         for line in output.split("\n"):
             match = re.match(r'([0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2})', line)
             if match:
-                hwaddr = match.groups()[0]
+                hwaddr = normalize_hwaddr(match.groups()[0])
                 match = re.match(r'.*\s+vlan (\d+)', line)
                 vlan_id = int(match.groups()[0]) if match else 0
                 self = True if re.match(r'.*\s+self', line) else False