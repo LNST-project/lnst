917849e3e1d67b909f807a9d3d777a1095fca590
Ido Schimmel <idosch@mellanox.com>
2016-01-12 11:18:06 +0200

    setup.py: Add Custom module
    Fixes: 30b3f0923de0 ("test_modules: introduce "Custom" test module")
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>

--- setup.py
+++ setup.py
@@ -125,7 +125,8 @@ TEST_MODULES = [
          "test_modules/PacketAssert.py",
          "test_modules/PktCounter.py",
          "test_modules/PktgenTx.py",
-         "test_modules/LinkNeg.py"]
+         "test_modules/LinkNeg.py",
+         "test_modules/Custom.py"]
     )
 ]
 
