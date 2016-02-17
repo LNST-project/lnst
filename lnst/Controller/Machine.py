LNST-project/lnst

commit af025186a329599e5ec70cf8c46dd61ec8f12fe4

InterfaceAPI: Introduce slave_{add, del}
It is useful for driver authors to be able to add or remove net devices
from certain master devices, so allow a user to do that from inside a
task.

This is currently only supported by bridge and team.

Acked-by: Jiri Pirko <jiri@mellanox.com>
Signed-off-by: Ido Schimmel <idosch@mellanox.com>
Signed-off-by: Jiri Pirko <jiri@mellanox.com>

lnst/Controller/Machine.py
@@ -806,6 +806,12 @@ class Interface(object):
     def set_autoneg(self):
         self._machine._rpc_call_x(self._netns, "set_autoneg", self._id)
 
+    def slave_add(self, if_id):
+        self._machine._rpc_call_x(self._netns, "slave_add", self._id, if_id)
+
+    def slave_del(self, if_id):
+        self._machine._rpc_call_x(self._netns, "slave_del", self._id, if_id)
+
 class StaticInterface(Interface):
     """ Static interface
 

lnst/Controller/Task.py
@@ -582,6 +582,12 @@ class InterfaceAPI(object):
     def set_autoneg(self):
         return self._if.set_autoneg()
 
+    def slave_add(self, slave_id):
+        return self._if.slave_add(slave_id)
+
+    def slave_del(self, slave_id):
+        return self._if.slave_del(slave_id)
+
 class ModuleAPI(object):
     """ An API class representing a module. """
 

lnst/Slave/InterfaceManager.py
@@ -550,3 +550,11 @@ class Device(object):
 
     def set_autoneg(self):
         exec_cmd("ethtool -s %s autoneg on" % self._name)
+
+    def slave_add(self, if_id):
+        if self._conf != None:
+            self._conf.slave_add(if_id)
+
+    def slave_del(self, if_id):
+        if self._conf != None:
+            self._conf.slave_del(if_id)

lnst/Slave/NetTestSlave.py
@@ -794,6 +794,24 @@ class SlaveMethods:
         self._if_manager.wait_interface_init()
         return True
 
+    def slave_add(self, if_id, slave_id):
+        dev = self._if_manager.get_mapped_device(if_id)
+        if dev is not None:
+            dev.slave_add(slave_id)
+        else:
+            logging.error("Device with id '%s' not found." % if_id)
+            return False
+        return True
+
+    def slave_del(self, if_id, slave_id):
+        dev = self._if_manager.get_mapped_device(if_id)
+        if dev is not None:
+            dev.slave_del(slave_id)
+        else:
+            logging.error("Device with id '%s' not found." % if_id)
+            return False
+        return True
+
 class ServerHandler(ConnectionHandler):
     def __init__(self, addr):
         super(ServerHandler, self).__init__()

commit a6e4e9f526233aa90e2e5897649990f20fd12862

lnst: Fix dynamic addition / deletion of slaves
The slaves of an interface are determined during creation time and
stored in two objects:
	* Interface on the controller
	* NetConfigDeviceGeneric on the slave machine

When slaves are dynamically added / deleted during a Python task the
'slaves' data structures in these objects are not updated, which can
cause several problems:

If a slave is removed from its master, then during cleanup the slave
machine will try to remove it again and thereby generate an error. This
requires us to update the data struct on the slave machine.

If a slave is added to a master and that master is later reset(), then
the newly added slave won't be configured correctly after the reset().
This requires us to update the data struct on the controller.

Cc: Andrew Lunn <andrew@lunn.ch>
Fixes: af025186a329 ("InterfaceAPI: Introduce slave_{add, del}")
Signed-off-by: Ido Schimmel <idosch@mellanox.com>
Signed-off-by: Jiri Pirko <jiri@mellanox.com>

lnst/Controller/Machine.py
@@ -591,6 +591,12 @@ class Interface(object):
             else:
                 self._master["other"].append(master)
 
+    def del_master(self, master):
+        if self._master["primary"] is master:
+            self._master["primary"] = None
+        else:
+            self._master["other"].remove(master)
+
     def get_primary_master(self):
         return self._master["primary"]
 
@@ -600,6 +606,10 @@ class Interface(object):
             iface.add_master(self, primary=False)
         else:
             iface.add_master(self)
+
+    def del_slave(self, iface):
+        iface.del_master(self)
+        del self._slaves[iface.get_id()]
 
     def set_slave_option(self, slave_id, name, value):
         if slave_id not in self._slave_options:
@@ -812,8 +822,10 @@ class Interface(object):
 
     def slave_add(self, if_id):
         self._machine._rpc_call_x(self._netns, "slave_add", self._id, if_id)
+        self.add_slave(self._machine.get_interface(if_id))
 
     def slave_del(self, if_id):
+        self.del_slave(self._machine.get_interface(if_id))
         self._machine._rpc_call_x(self._netns, "slave_del", self._id, if_id)
 
 class StaticInterface(Interface):

lnst/Slave/NetConfigDevice.py
@@ -212,8 +212,10 @@ class NetConfigDeviceBridge(NetConfigDeviceGeneric):
 
     def slave_add(self, slave_id):
         self._add_rm_port("add", slave_id)
+        self._dev_config["slaves"].append(slave_id)
 
     def slave_del(self, slave_id):
+        self._dev_config["slaves"].remove(slave_id)
         self._add_rm_port("del", slave_id)
 
 class NetConfigDeviceMacvlan(NetConfigDeviceGeneric):
@@ -379,14 +381,14 @@ class NetConfigDeviceTeam(NetConfigDeviceGeneric):
         self._ports_down()
 
         for slave_id in get_slaves(self._dev_config):
-            self.slave_add(slave_id)
+            self._slave_add(slave_id)
         self._ports_up()
 
     def deconfigure(self):
         for slave_id in get_slaves(self._dev_config):
-            self.slave_del(slave_id)
+            self._slave_del(slave_id)
 
-    def slave_add(self, slave_id):
+    def _slave_add(self, slave_id):
         dev_name = self._dev_config["name"]
         port_dev = self._if_manager.get_mapped_device(slave_id)
         port_name = port_dev.get_name()
@@ -400,12 +402,20 @@ class NetConfigDeviceTeam(NetConfigDeviceGeneric):
         port_dev.down()
         exec_cmd("teamdctl %s %s port add %s" % (dbus_option, dev_name, port_name))
 
-    def slave_del(self, slave_id):
+    def slave_add(self, slave_id):
+        self._slave_add(slave_id)
+        self._dev_config["slaves"].append(slave_id)
+
+    def _slave_del(self, slave_id):
         dev_name = self._dev_config["name"]
         port_dev = self._if_manager.get_mapped_device(slave_id)
         port_name = port_dev.get_name()
         dbus_option = "-D" if self._should_enable_dbus() else ""
         exec_cmd("teamdctl %s %s port remove %s" % (dbus_option, dev_name, port_name))
+
+    def slave_del(self, slave_id):
+        self._dev_config["slaves"].remove(slave_id)
+        self._slave_del(slave_id)
 
 class NetConfigDeviceOvsBridge(NetConfigDeviceGeneric):
     _modulename = "openvswitch"