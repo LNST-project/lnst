LNST-project/lnst

commit bc9c6296e434aa3c557e5f25ca5fb973993fc3e7

cmd: allow to parse JSON stdout into dictionary
This is handy for processing output of commands producing JSON.

Signed-off-by: Jiri Pirko <jiri@mellanox.com>

lnst/Common/NetTestCommand.py
@@ -407,7 +407,8 @@ class NetTestCommandExec(NetTestCommandGeneric):
                 stdout, stderr = self.exec_from(self._command["from"],
                                                 self._command["command"])
             else:
-                stdout, stderr = self.exec_cmd(self._command["command"])
+                json = True if "json" in self._command else False
+                stdout, stderr = self.exec_cmd(self._command["command"], json=json)
             res_data = {"stdout": stdout, "stderr": stderr}
             self.set_pass(res_data)
         except ExecCmdFail as e:

lnst/Common/ExecCmd.py
@@ -51,7 +51,7 @@ def log_output(log_func, out_type, out):
              "----------------------------"
              % (out_type, out))
 
-def exec_cmd(cmd, die_on_err=True, log_outputs=True, report_stderr=False):
+def exec_cmd(cmd, die_on_err=True, log_outputs=True, report_stderr=False, json=False):
     cmd = cmd.rstrip(" ")
     logging.debug("Executing: \"%s\"" % cmd)
     subp = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
@@ -71,5 +71,9 @@ def exec_cmd(cmd, die_on_err=True, log_outputs=True, report_stderr=False):
         err = ExecCmdFail(cmd, subp.returncode, [data_stdout, data_stderr], report_stderr)
         logging.error(err)
         raise err
+
+    if json:
+        import json
+        data_stdout = json.loads(data_stdout)
 
     return data_stdout, data_stderr

lnst/Controller/Task.py
@@ -242,6 +242,8 @@ class HostAPI(object):
             :type timeout: int
             :param tool: Run from a tool (the same as 'from' in XML).
             :type tool: string
+            :param json: Process JSON output into dictionary.
+            :type json: bool
 
             :return: A handle for process.
             :rtype: ProcessAPI
@@ -283,6 +285,8 @@ class HostAPI(object):
                 cmd["netns"] = argval
             elif arg == "save_output":
                 pass # now ignored as output is saved always
+            elif arg == "json":
+                cmd["json"] = argval
             else:
                 msg = "Argument '%s' not recognised by the run() method." % arg
                 raise TaskError(msg)

commit 3257b244ac1de184b36c914fd35e053074d7d50a

NetTestCommand: fix requested json output check
Fixes: bc9c6296e43 ("cmd: allow to parse JSON stdout into dictionary")
Signed-off-by: Jiri Pirko <jiri@mellanox.com>

lnst/Common/NetTestCommand.py
@@ -407,7 +407,7 @@ class NetTestCommandExec(NetTestCommandGeneric):
                 stdout, stderr = self.exec_from(self._command["from"],
                                                 self._command["command"])
             else:
-                json = True if "json" in self._command else False
+                json = True if "json" in self._command and self._command["json"] else False
                 stdout, stderr = self.exec_cmd(self._command["command"], json=json)
             res_data = {"stdout": stdout, "stderr": stderr}
             self.set_pass(res_data)