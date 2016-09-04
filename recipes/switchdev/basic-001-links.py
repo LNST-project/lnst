80b60096df601d9eb9d99951d338c53a95864975
Ido Schimmel <idosch@mellanox.com>
2016-09-04 14:38:09 +0300

    recipes: switchdev: Use only two interfaces to test LinkNeg
    We should only specify two interfaces for the link negotiation recipe.
    We can use 'multi-match' to test all possible matches.
    
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>

--- recipes/switchdev/basic-001-links.py
+++ recipes/switchdev/basic-001-links.py
@@ -31,35 +31,20 @@ def linkneg(tl, if1, if2):
         tl.ping_simple(if1, if2)
 
 def do_task(ctl, hosts, ifaces, aliases):
-    m1, sw = hosts
-    m1_if1, m1_if2, m1_if3, m1_if4, sw_if1, sw_if2, sw_if3, sw_if4 = ifaces
+    m1_if1, sw_if1 = ifaces
 
     m1_if1.reset(ip=["192.168.101.10/24", "2002::1/64"])
-    m1_if2.reset(ip=["192.168.102.10/24", "2003::1/64"])
-    m1_if3.reset(ip=["192.168.103.10/24", "2004::1/64"])
-    m1_if4.reset(ip=["192.168.104.10/24", "2005::1/64"])
     sw_if1.reset(ip=["192.168.101.11/24", "2002::2/64"])
-    sw_if2.reset(ip=["192.168.102.11/24", "2003::2/64"])
-    sw_if3.reset(ip=["192.168.103.11/24", "2004::2/64"])
-    sw_if4.reset(ip=["192.168.104.11/24", "2005::2/64"])
 
     sleep(30)
 
     tl = TestLib(ctl, aliases)
 
-    for (if1, if2) in [(sw_if1, m1_if1), (sw_if2, m1_if2), (sw_if3, m1_if3),
-                       (sw_if4, m1_if4)]:
-        linkneg(tl, if1, if2)
-        linkneg(tl, if2, if1)
+    linkneg(tl, sw_if1, m1_if1)
+    linkneg(tl, m1_if1, sw_if1)
 
 do_task(ctl, [ctl.get_host("machine1"),
               ctl.get_host("switch")],
         [ctl.get_host("machine1").get_interface("if1"),
-         ctl.get_host("machine1").get_interface("if2"),
-         ctl.get_host("machine1").get_interface("if3"),
-         ctl.get_host("machine1").get_interface("if4"),
-         ctl.get_host("switch").get_interface("if1"),
-         ctl.get_host("switch").get_interface("if2"),
-         ctl.get_host("switch").get_interface("if3"),
-         ctl.get_host("switch").get_interface("if4")],
+         ctl.get_host("switch").get_interface("if1")],
         ctl.get_aliases())
