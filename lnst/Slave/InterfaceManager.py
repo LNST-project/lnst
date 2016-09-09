LNST-project/lnst

commit 49e523d5aef46220968c9c4ed2adcf4b47598329

Slave: Add basic support for tc.
Add the tc filter clear and tc qdisc clear commands to the Interface
clear_configuration of the InterfaceManager.

Signed-off-by: Yotam Gigi <yotamg@mellanox.com>
Signed-off-by: Jiri Pirko <jiri@mellanox.com>

lnst/Slave/InterfaceManager.py
@@ -519,6 +519,32 @@ class Device(object):
         self._conf = None
         self._conf_dict = None
 
+    def _clear_tc_qdisc(self):
+        exec_cmd("tc qdisc replace dev %s root pfifo" % self._name)
+        out, _ = exec_cmd("tc filter show dev %s" % self._name)
+        ingress_handles = re.findall("ingress (\\d+):", out)
+        for ingress_handle in ingress_handles:
+            exec_cmd("tc qdisc del dev %s handle %s: ingress" %
+                     (self._name, ingress_handle))
+        out, _ = exec_cmd("tc qdisc show dev %s" % self._name)
+        ingress_qdiscs = re.findall("qdisc ingress (\\w+):", out)
+        if len(ingress_qdiscs) != 0:
+                exec_cmd("tc qdisc del dev %s ingress" % self._name)
+
+    def _clear_tc_filters(self):
+        out, _ = exec_cmd("tc filter show dev %s" % self._name)
+        egress_prefs = re.findall("pref (\\d+) .* handle", out)
+        out, _ = exec_cmd("tc filter show dev %s ingress" % self._name)
+        ingress_prefs = re.findall("pref (\\d+) .* handle", out)
+
+        for egress_pref in egress_prefs:
+            exec_cmd("tc filter del dev %s pref %s" % (self._name,
+                     egress_pref))
+
+        for ingress_pref in ingress_prefs:
+            exec_cmd("tc filter del dev %s pref %s ingress" % (self._name,
+                     ingress_pref))
+
     def clear_configuration(self):
         if self._master["primary"]:
             primary_id = self._master["primary"]
@@ -532,6 +558,8 @@ class Device(object):
                 m_dev.clear_configuration()
 
         if self._conf != None:
+            self._clear_tc_qdisc()
+            self._clear_tc_filters()
             self.down()
             self.deconfigure()
             self.destroy()

commit c5a63889a82a00b2a9ea4c1bcdb732aafc0f6de8

InterfaceManager: fix tc deconfiguration
The recently added functionality of clearing tc configuration broke
recipes that already used netem functionality directly from the
configuration XML part. The problem was with trying to clear tc
configuration twice.

To fixed this I moved the new functions to be called after deconfigure()
takes care of it's own tc configuration. For this to work I also had to
add a check for interface existence since deconfigure() can remove the
device completely if NetworkManager is used.

Fixes: 49e523d5 ("Slave: Add basic support for tc.")
Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

lnst/Slave/InterfaceManager.py
@@ -520,6 +520,13 @@ class Device(object):
         self._conf_dict = None
 
     def _clear_tc_qdisc(self):
+        try:
+            #checks device existence as it might have been removed by
+            #calling self.deconfigure()
+            exec_cmd("ip l show %s" % self._name, log_outputs=False)
+        except:
+            return
+
         exec_cmd("tc qdisc replace dev %s root pfifo" % self._name)
         out, _ = exec_cmd("tc filter show dev %s" % self._name)
         ingress_handles = re.findall("ingress (\\d+):", out)
@@ -532,6 +539,13 @@ class Device(object):
                 exec_cmd("tc qdisc del dev %s ingress" % self._name)
 
     def _clear_tc_filters(self):
+        try:
+            #checks device existence as it might have been removed by
+            #calling self.deconfigure()
+            exec_cmd("ip l show %s" % self._name, log_outputs=False)
+        except:
+            return
+
         out, _ = exec_cmd("tc filter show dev %s" % self._name)
         egress_prefs = re.findall("pref (\\d+) .* handle", out)
 
@@ -552,10 +566,10 @@ class Device(object):
                 m_dev.clear_configuration()
 
         if self._conf != None:
-            self._clear_tc_qdisc()
-            self._clear_tc_filters()
             self.down()
             self.deconfigure()
+            self._clear_tc_qdisc()
+            self._clear_tc_filters()
             self.destroy()
             self._conf = None
             self._conf_dict = None