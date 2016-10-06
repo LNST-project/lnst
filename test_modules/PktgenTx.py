LNST-project/lnst

commit d2bee9f4d80a8c13e0d6cd7c08cc48293dccd460

PktgenTx: Remove all devices from all threads in init
The PktgenTx module removes devices from the pktgen threads that are
going to participate in the recipe. However, when 'start' is issued all
the configured pktgen threads will start generating packets, even those
configured prior to the currently executing recipe.

Fix this by deconfiguring all the pktgen threads during the module init.

Signed-off-by: Ido Schimmel <idosch@mellanox.com>
Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

test_modules/PktgenTx.py
@@ -32,7 +32,6 @@ class PktgenWorkers:
     def _init_current_wrkr(self):
         num = self._current
         wrkr = Pktgen("/proc/net/pktgen/kpktgend_%d" % (num))
-        wrkr.set("rem_device_all")
         wrkr.set("max_before_softirq 5000")
         self._wrkrs[num] = wrkr
 
@@ -61,6 +60,11 @@ def pktget_options_merge(pktgen_options, default_pktgen_options):
     res = res + opts
     return [" ".join(opt) for opt in res]
 
+def pktgen_devices_remove():
+    for cpu in range(os.sysconf('SC_NPROCESSORS_ONLN')):
+        cmd = "echo rem_device_all > /proc/net/pktgen/kpktgend_{}"
+        exec_cmd(cmd.format(cpu))
+
 class PktgenTx(TestGeneric):
     def run(self):
         dev_names = self.get_multi_mopt("netdev_name")
@@ -76,6 +80,8 @@ class PktgenTx(TestGeneric):
                                               default_pktgen_options)
 
         exec_cmd("modprobe pktgen")
+
+        pktgen_devices_remove()
 
         pgctl = Pktgen("/proc/net/pktgen/pgctrl")
         pgwrkr = PktgenWorkers()

commit 83075785e7e430b08355ec994a731649670dd9bd

modules: fix PktgenTx to work with python2.6
RHEL6 has older python version 2.6.6 and that version requires an index in
formatting string, e.g. {0}

For python versions 2.7 and above the index can be omitted.

Fixes: d2bee9f ("PktgenTx: Remove all devices from all threads in init")

Signed-off-by: Jan Tluka <jtluka@redhat.com>
Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

test_modules/PktgenTx.py
@@ -62,7 +62,7 @@ def pktget_options_merge(pktgen_options, default_pktgen_options):
 
 def pktgen_devices_remove():
     for cpu in range(os.sysconf('SC_NPROCESSORS_ONLN')):
-        cmd = "echo rem_device_all > /proc/net/pktgen/kpktgend_{}"
+        cmd = "echo rem_device_all > /proc/net/pktgen/kpktgend_{0}"
         exec_cmd(cmd.format(cpu))
 
 class PktgenTx(TestGeneric):