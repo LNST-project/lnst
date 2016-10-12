LNST-project/lnst

commit eca3a19a12d7562f4c4ad17124d2d6dfd70a6018

Netperf: add option max_deviation
This option limits the maximum allowed deviation. If the measured
deviation exceeds this limit the result will automatically be FAIL.
This is because sometimes the deviation gets so big that it overlaps
with the specified threshold interval resulting in a PASS even though
the result is so unstable that it shouldn't be compared to anything.

The option accepts either a percentual value describing the maximum
deviation or an absolute value in the same format that we use for
specifying the threshold.

Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>
Acked-by: Jan Tluka <jtluka@redhat.com>

test_modules/Netperf.py
@@ -56,6 +56,9 @@ class Netperf(TestGeneric):
         else:
             self._threshold_interval = None
 
+        self._max_deviation = self._parse_max_deviation(
+                                self.get_opt("max_deviation", default=None))
+
     def _is_omni(self):
         return self._testname in self.omni_tests
 
@@ -313,6 +316,21 @@ class Netperf(TestGeneric):
         return {"rate": threshold_rate,
                 "unit": threshold_unit_type}
 
+    def _parse_max_deviation(self, deviation):
+        if deviation is None:
+            return None
+        percentual_deviation = r"(\d+(.\d+)?)\s*%"
+        match = re.match(percentual_deviation, deviation)
+        if match:
+            return {"type": "percent",
+                    "value": float(match.group(1))}
+        else:
+            val = self._parse_threshold(deviation)
+            if val is not None:
+                return {"type": "absolute",
+                        "value": val}
+        return None
+
     def _sum_results(self, first, second):
         result = {}
 
@@ -457,6 +475,30 @@ class Netperf(TestGeneric):
         rate_dev_pretty = self._pretty_rate(rate_deviation, unit=rate_pretty["unit"])
 
         res_val = False
+        if self._max_deviation is not None:
+            if self._max_deviation["type"] == "percent":
+                percentual_deviation = (rate_deviation / rate) * 100
+                if percentual_deviation > self._max_deviation["value"]:
+                    res_val = False
+                    res_data["msg"] = "Measured rate %.2f +-%.2f %s has bigger "\
+                                      "deviation than allowed (+-%.2f %%)" %\
+                                      (rate_pretty["rate"],
+                                       rate_dev_pretty["rate"],
+                                       rate_pretty["unit"],
+                                       self._max_deviation["value"])
+                    return (res_val, res_data)
+            elif self._max_deviation["type"] == "absolute":
+                if rate_deviation > self._max_deviation["value"]["rate"]:
+                    pretty_deviation = self._pretty_rate(self._max_deviation["value"]["rate"])
+                    res_val = False
+                    res_data["msg"] = "Measured rate %.2f +-%.2f %s has bigger "\
+                                      "deviation than allowed (+-%.2f %s)" %\
+                                      (rate_pretty["rate"],
+                                       rate_dev_pretty["rate"],
+                                       rate_pretty["unit"],
+                                       pretty_deviation["rate"],
+                                       pretty_deviation["unit"])
+                    return (res_val, res_data)
         if self._threshold_interval is not None:
             result_interval = (rate - rate_deviation,
                                rate + rate_deviation)

commit 9a1d03186daac2136ae56b446d79f200c8788a46

Netperf: fix division by zero when reporting results
When measured rate was 0, a division by zero can occur while checking if
the result deviation was too high. This commit fixes the issue, by
automatically rejecting 0 throughput results as FAIL.

Fixes: eca3a19a12 ("Netperf: add option max_deviation")
Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>
Acked-by: Jan Tluka <jtluka@redhat.com>

test_modules/Netperf.py
@@ -502,6 +502,16 @@ class Netperf(TestGeneric):
             return (False, res_data)
 
         res_val = False
+        res_data["msg"] = "Measured rate was %.2f +-%.2f %s" %\
+                                            (rate_pretty["rate"],
+                                             rate_dev_pretty["rate"],
+                                             rate_pretty["unit"])
+        if rate > 0.0:
+            res_val = True
+        else:
+            res_val = False
+            return (res_val, res_data)
+
         if self._max_deviation is not None:
             if self._max_deviation["type"] == "percent":
                 percentual_deviation = (rate_deviation / rate) * 100
@@ -556,15 +566,6 @@ class Netperf(TestGeneric):
                                    threshold_dev_pretty["rate"],
                                    threshold_pretty["unit"])
                 return (res_val, res_data)
-        else:
-            if rate > 0.0:
-                res_val = True
-            else:
-                res_val = False
-            res_data["msg"] = "Measured rate was %.2f +-%.2f %s" %\
-                                                (rate_pretty["rate"],
-                                                 rate_dev_pretty["rate"],
-                                                 rate_pretty["unit"])
         return (res_val, res_data)
 
     def run(self):