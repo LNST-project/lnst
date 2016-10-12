LNST-project/lnst

commit ea75135ce129cbb4cb6b242230be8503d0f4285d

Netperf: add option udp_size
This option set UDP datagram size for netperf test on client. If this option
is not explicitly specified, default value from netperf is used.

The option accepts integer value describing datagram size, it should be also
concatenated with G/M/K/g/m/k.

In this commit I also added this option to regression_test: simple_netperf test.

Signed-off-by: Kamil Jerabek <kjerabek@redhat.com>
Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

test_modules/Netperf.py
@@ -39,6 +39,7 @@ class Netperf(TestGeneric):
         self._cpu_util = self.get_opt("cpu_util")
         self._num_parallel = int(self.get_opt("num_parallel", default=1))
         self._runs = self.get_opt("runs", default=1)
+        self._udp_size = self.get_opt("udp_size")
         self._debug = int_it(self.get_opt("debug", default=0))
 
         self._threshold = self._parse_threshold(self.get_opt("threshold"))
@@ -140,6 +141,16 @@ class Netperf(TestGeneric):
                     cmd += " %s" % self._testoptions
                 else:
                     cmd += " -- %s" % self._testoptions
+
+            if self._udp_size is not None:
+                """
+                udp packets will have this size
+                """
+                if self._is_omni() or self._testoptions:
+                    cmd += " -m %s" % self._udp_size
+                else:
+                    cms += " -- -m %s" % self._udp_size
+
 
         elif self._role == "server":
             cmd = "netserver -D"

commit 4abab6085a64c1ff9e91ca11f809a4fa47af9653

Netperf: fix typo in parsing udp_size param
Fixes: ea75135c ("Netperf: add option udp_size")
Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>
Acked-by: Jan Tluka <jtluka@redhat.com>

test_modules/Netperf.py
@@ -149,7 +149,7 @@ class Netperf(TestGeneric):
                 if self._is_omni() or self._testoptions:
                     cmd += " -m %s" % self._udp_size
                 else:
-                    cms += " -- -m %s" % self._udp_size
+                    cmd += " -- -m %s" % self._udp_size
 
 
         elif self._role == "server":