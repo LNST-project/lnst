f0ff7ac8abcfb47ef6e6bf3b7ad1c88da31ca456
Ondrej Lichtner <olichtne@redhat.com>
2017-05-05 14:39:48 +0200

    add lnst.Common.DeviceRef
    Defines the DeviceRef class that will be used by the Controller-Slave
    communication protocol.
    
    Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

---
+++ lnst/Common/DeviceRef.py
@@ -0,0 +1,19 @@
+"""
+Defines the DeviceRef class.
+
+Copyright 2017 Red Hat, Inc.
+Licensed under the GNU General Public License, version 2 as
+published by the Free Software Foundation; see COPYING for details.
+"""
+
+__author__ = """
+olichtne@redhat.com (Ondrej Lichtner)
+"""
+
+class DeviceRef(object):
+    """Device reference transferable over network
+
+    Used in Controller-Slave commucation protocol.
+    """
+    def __init__(self, if_index):
+        self.if_index = int(if_index)
