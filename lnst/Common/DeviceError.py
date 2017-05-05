b3940fe6e14d510b53b7268374c1f23d16a071a0
Ondrej Lichtner <olichtne@redhat.com>
2017-05-05 14:29:06 +0200

    add lnst.Common.DeviceError
    Defines some basic Exceptions signaling some error with a Device object
    (to be added in a later commit). Since these Exceptions can be
    transmitted to the Controller, they need to be in the Common package.
    
    Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

---
+++ lnst/Common/DeviceError.py
@@ -0,0 +1,22 @@
+"""
+Defines the DeviceError, DeviceDeleted and DeviceNotFound exceptions.
+
+Copyright 2017 Red Hat, Inc.
+Licensed under the GNU General Public License, version 2 as
+published by the Free Software Foundation; see COPYING for details.
+"""
+
+__author__ = """
+olichtne@redhat.com (Ondrej Lichtner)
+"""
+
+from lnst.Common.LnstError import LnstError
+
+class DeviceError(LnstError):
+    pass
+
+class DeviceDeleted(DeviceError):
+    pass
+
+class DeviceNotFound(DeviceError):
+    pass
