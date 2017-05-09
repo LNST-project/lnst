494b79d5d60467b50535d8c18d4ed1758daa2c2d
Ondrej Lichtner <olichtne@redhat.com>
2017-05-09 11:24:15 +0200

    add lnst.Common.JobError
    This module defines the common JobError exception that will be used by
    both the Controller and the Slave Job classes.
    
    Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

---
+++ lnst/Common/JobError.py
@@ -0,0 +1,22 @@
+"""
+This module defines the common JobError exception used by both the Controller
+and the Slave
+
+Copyright 2017 Red Hat, Inc.
+Licensed under the GNU General Public License, version 2 as
+published by the Free Software Foundation; see COPYING for details.
+"""
+
+__author__ = """
+olichtne@redhat.com (Ondrej Lichtner)
+"""
+
+from lnst.Common.LnstError import LnstError
+
+class JobError(LnstError):
+    """Base class for client errors."""
+    def __init__(self, s):
+        self._s = s
+
+    def __str__(self):
+        return "JobError: " + str(self._s)
