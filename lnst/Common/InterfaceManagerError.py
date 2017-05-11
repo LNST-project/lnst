fa7141ad47e4ea056d85428a2fbdc4c1d67a7677
Ondrej Lichtner <olichtne@redhat.com>
2017-05-11 09:30:16 +0200

    add lnst.Common.InterfaceManagerError
    Defines the Exception class for errors from the InterfaceManager.
    
    Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

---
+++ lnst/Common/InterfaceManagerError.py
@@ -0,0 +1,16 @@
+"""
+Defines the InterfaceManagerError exception class.
+
+Copyright 2017 Red Hat, Inc.
+Licensed under the GNU General Public License, version 2 as
+published by the Free Software Foundation; see COPYING for details.
+"""
+
+__author__ = """
+olichtne@redhat.com (Ondrej Lichtner)
+"""
+
+from lnst.Common.LnstError import LnstError
+
+class InterfaceManagerError(LnstError):
+    pass
