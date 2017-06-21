078b900ce44ff156cbb72b649ccf15355badbd86
Ondrej Lichtner <olichtne@redhat.com>
2017-06-21 15:57:21 +0200

    Controller package: export NetNamespace class
    The NetNamespace class should be directly accessible to the tester so we
    can export it in the main lnst.Controller package for easier importing.
    
    Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

--- lnst/Controller/__init__.py
+++ lnst/Controller/__init__.py
@@ -1,3 +1,4 @@
 from lnst.Controller.Controller import Controller
 from lnst.Controller.Recipe import BaseRecipe
 from lnst.Controller.Requirements import HostReq, DeviceReq
+from lnst.Controller.NetNamespace import NetNamespace
