052a3d4148d1eea870d48a141918b03267e2dd0f
Ondrej Lichtner <olichtne@redhat.com>
2017-06-21 15:42:47 +0200

    add lnst.Controller.NetNamespace
    Defines the NetNamespace class that represents the API for a network namespace
    of a slave machine. It's derived from the base Namespace class and it
    very simply just overrides the __init__ method to allow for instances of
    this class to be created by the tester, not just LNST internals.
    
    Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

---
+++ lnst/Controller/NetNamespace.py
@@ -0,0 +1,31 @@
+"""
+Defines the NetNamespace class that represents the API for a network namespace
+of a slave machine.
+
+Copyright 2017 Red Hat, Inc.
+Licensed under the GNU General Public License, version 2 as
+published by the Free Software Foundation; see COPYING for details.
+"""
+
+__author__ = """
+olichtne@redhat.com (Ondrej Lichtner)
+"""
+
+import logging
+from lnst.Common.NetTestCommand import DEFAULT_TIMEOUT
+from lnst.Devices.VirtualDevice import VirtualDevice
+from lnst.Devices.RemoteDevice import RemoteDevice
+from lnst.Controller.Namespace import Namespace
+from lnst.Controller.Namespace import HostError
+
+class NetNamespace(Namespace):
+    """Namespace derived class for a network namespace
+
+    Created by the tester, should be assigned to a Host object which will
+    perform the namespace creation. After that the tester uses it the same
+    way."""
+    def __init__(self, name):
+        super(NetNamespace, self).__init__(None)
+
+        self._nsname = name
+        #self.jobs = None #TODO
