LNST-project/lnst

commit 5498ba20cd6e437ccc2413d0efd4fdbc8a8b4bd4

SlavePool: properly close sockets after connection check
The sockets should be properly closed after the availability of the
lnst-slave is checked. This adds the relevant calls.

Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

lnst/Controller/SlavePool.py
@@ -103,9 +103,12 @@ class SlavePool:
                     m_id = check_sockets[s]
                     if err == 0:
                         pool[m_id]["available"] = True
+                        s.shutdown(socket.SHUT_RDWR)
+                        s.close()
                         del check_sockets[s]
                     else:
                         pool[m_id]["available"] = False
+                        s.close()
                         del check_sockets[s]
         else:
             for m_id in pool.keys():

commit c54a38eafea6e9e784cddd14deaac0e3abb4b5f7

SlavePool: fix unresolvable hostname exception
If LNST controller processes a pool that contains a machine with a hostname
that can't be resolved by DNS the controller fails with unhandled exception.
The exception is raised on shutdown of the socket used to connect to the
machine. Since the connection on the socket did not succeed the attempt to
shutdown the connection is an error. Unfortunately this error is not detected
by checking SO_ERR in getsockopt() after calling select() on the socket.

The fix is to mark the machine unavailable immediately unless the error code
raised by connect call is EINPROGRESS meaning the connection proceeded to
establishing phase (the hostname was succesfully resolved).

Fixes: 5498ba2 ("SlavePool: properly close sockets after connection check")
Fixes issue #188

Signed-off-by: Jan Tluka <jtluka@redhat.com>
Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

lnst/Controller/SlavePool.py
@@ -15,6 +15,7 @@ rpazdera@redhat.com (Radek Pazdera)
 
 import logging
 import os
+import errno
 import re
 import socket
 import select
@@ -104,8 +105,21 @@ class SlavePool:
                 s.settimeout(0)
                 try:
                     s.connect((hostname, port))
-                except:
-                    pass
+                except socket.error as msg:
+                    # if the error is other than EINPROGRESS, e.g. the stack
+                    # could not resolve name, the machine should become unavailable
+                    try:
+                        en = msg.errno
+                    except AttributeError:
+                        en = 0
+
+                    if en != errno.EINPROGRESS:
+                        pool[m_id]["available"] = False
+                        s.close()
+                        logging.debug("Bypassing machine '%s' (%s)" %
+                            (m_id, msg))
+                        continue
+
                 check_sockets[s] = m_id
 
             while len(check_sockets) > 0: