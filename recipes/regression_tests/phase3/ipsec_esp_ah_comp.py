57b4445363964ffa0aa7dc953eb96796a46c603a
Kamil Jerabek <kjerabek@redhat.com>
2017-09-11 13:06:07 +0200

    regression_tests: ipsec_esp_ah_comp add netperf result parameter ipsec_mode
    This commit assigns ipsec_mode parameter to each netperf result.
    
    Signed-off-by: Kamil Jerabek <kjerabek@redhat.com>
    Signed-off-by: Jan Tluka <jtluka@redhat.com>

--- recipes/regression_tests/phase3/ipsec_esp_ah_comp.py
+++ recipes/regression_tests/phase3/ipsec_esp_ah_comp.py
@@ -332,6 +332,7 @@ for ciph_alg, ciph_len in ciphers:
             result_tcp.set_parameter('cipher_len', ciph_len)
             result_tcp.set_parameter('hash_alg', hash_alg)
             result_tcp.set_parameter('msg_size', nperf_msg_size)
+            result_tcp.set_parameter('ipsec_mode', ipsec_mode)
 
             baseline = perf_api.get_baseline_of_result(result_tcp)
             baseline = perfrepo_baseline_to_dict(baseline)
@@ -372,6 +373,7 @@ for ciph_alg, ciph_len in ciphers:
             result_udp.set_parameter('cipher_len', ciph_len)
             result_udp.set_parameter('hash_alg', hash_alg)
             result_udp.set_parameter('msg_size', nperf_msg_size)
+            result_udp.set_parameter('ipsec_mode', ipsec_mode)
 
             baseline = perf_api.get_baseline_of_result(result_udp)
             baseline = perfrepo_baseline_to_dict(baseline)
@@ -482,6 +484,7 @@ for ciph_alg, ciph_len in ciphers:
             result_tcp.set_parameter('cipher_len', ciph_len)
             result_tcp.set_parameter('hash_alg', hash_alg)
             result_tcp.set_parameter('msg_size', nperf_msg_size)
+            result_tcp.set_parameter('ipsec_mode', ipsec_mode)
 
             baseline = perf_api.get_baseline_of_result(result_tcp)
             baseline = perfrepo_baseline_to_dict(baseline)
@@ -522,6 +525,7 @@ for ciph_alg, ciph_len in ciphers:
             result_udp.set_parameter('cipher_len', ciph_len)
             result_udp.set_parameter('hash_alg', hash_alg)
             result_udp.set_parameter('msg_size', nperf_msg_size)
+            result_udp.set_parameter('ipsec_mode', ipsec_mode)
 
             baseline = perf_api.get_baseline_of_result(result_udp)
             baseline = perfrepo_baseline_to_dict(baseline)
