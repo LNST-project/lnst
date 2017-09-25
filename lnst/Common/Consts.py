5c34ee25135a49790df0cbdce40fd5cba94640c4
Nogah Frankel <nogahf@mellanox.com>
2017-09-25 16:40:23 +0300

    common: add a consts file for mrouter port related consts
    Add a Consts file to Common that holds classes that have only constants
    values and no methods.
    Add there a class for L2 multicast-router port related consts.
    
    Signed-off-by: Nogah Frankel <nogahf@mellanox.com>
    Reviewed-by: Yotam Gigi <yotamg@mellanox.com>
    Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

---
+++ lnst/Common/Consts.py
@@ -0,0 +1,16 @@
+"""
+General perpuses constants file
+
+Copyright 2016-2017 Mellanox Technologies. All rights reserved.
+Licensed under the GNU General Public License, version 2 as
+published by the Free Software Foundation; see COPYING for details.
+"""
+
+__autor__ = """
+nogahf@mellanox.com (Nogah Frankel)
+"""
+
+class MCAST_ROUTER_PORT:
+    FIXED_OFF = 0
+    LEARNING = 1
+    FIXED_ON = 2
