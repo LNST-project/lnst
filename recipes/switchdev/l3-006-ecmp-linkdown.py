7e92344bd2d535f92ad73560a3dcb5c63b77618f
Ido Schimmel <idosch@mellanox.com>
2017-10-29 16:05:14 +0200

    recipes: switchdev: Redue ECMP to two links
    The purpose of the recipes is to test ECMP, so we can do with just two
    links instead of four.
    
    This allows us to run the recipes on more setups without changing their
    functionality.
    
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Signed-off-by: Yuval Mintz <yuvalm@mellanox.com>
    Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

--- recipes/switchdev/l3-006-ecmp-linkdown.py
+++ recipes/switchdev/l3-006-ecmp-linkdown.py
@@ -25,16 +25,15 @@ def ipv4(test_ip):
 
 def do_task(ctl, hosts, ifaces, aliases):
     m1, sw, m2 = hosts
-    m1_if1, sw_if1, sw_if2, sw_if3, sw_if4, sw_if5, m2_if1, m2_if2, m2_if3, \
-        m2_if4, m2_if5, m3_if1 = ifaces
+    m1_if1, sw_if1, sw_if2, sw_if3, m2_if1, m2_if2, m2_if3, m3_if1 = ifaces
 
-    ecmp_sw_ifaces = [sw_if2, sw_if3, sw_if4, sw_if5]
-    ecmp_m_ifaces = [m2_if1, m2_if2, m2_if3, m2_if4]
+    ecmp_sw_ifaces = [sw_if2, sw_if3]
+    ecmp_m_ifaces = [m2_if1, m2_if2]
 
     m2.config("/proc/sys/net/ipv4/ip_forward", "1")
 
     ecmp_common.create_topology(m1_if1, sw_if1, ecmp_sw_ifaces, ecmp_m_ifaces,
-                                m2_if5, m3_if1)
+                                m2_if3, m3_if1)
     sleep(30)
 
     tl = TestLib(ctl, aliases)
@@ -90,12 +89,8 @@ do_task(ctl, [ctl.get_host("machine1"),
          ctl.get_host("switch").get_interface("if1"),
          ctl.get_host("switch").get_interface("if2"),
          ctl.get_host("switch").get_interface("if3"),
-         ctl.get_host("switch").get_interface("if4"),
-         ctl.get_host("switch").get_interface("if5"),
          ctl.get_host("machine2").get_interface("if1"),
          ctl.get_host("machine2").get_interface("if2"),
-         ctl.get_host("machine2").get_interface("if3"),
-         ctl.get_host("machine2").get_interface("if4"),
          ctl.get_host("machine2").get_interface("veth0"),
          ctl.get_host("machine2").get_interface("veth1")],
         ctl.get_aliases())
