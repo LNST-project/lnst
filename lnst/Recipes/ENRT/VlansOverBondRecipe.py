c1955b4b15fc7e9107026006f336ac938bd49f59
Christos Sfakianakis <csfakian@redhat.com>
2018-09-05 17:03:25 +0200

    lnst.Recipes.ENRT: add VlansOverBondRecipe
    Add ported recipe Vlans3Over{ActiveBackup,RoundRobin}BondRecipe,
    implementing old regression_tests/phase1/3_vlans_over_{active_
    backup,round_robin}_bond.xml. Its structure is based on previously
    ported SimplePerfRecipe. Due to limitations in the current
    EnrtConfiguration class, a single vlan test pair is chosen.
    
    v2: Merge Vlans3Over{ActiveBackup, RoundRobin}BondRecipe into
    VlansOverBondRecipe. Add bonding mode and miimon value as
    parameters. Keep 2 vlans.
    
    olichtne: fixed trailing whitespace error
    
    Signed-off-by: Christos Sfakianakis <csfakian@redhat.com>
    Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

---
+++ lnst/Recipes/ENRT/VlansOverBondRecipe.py
@@ -0,0 +1,88 @@
+"""
+Implements scenario similar to regression_tests/phase1/
+(3_vlans_over_{round_robin, active_backup}_bond.xml + 3_vlans_over_bond.py),
+but 2 Vlans are used
+"""
+from lnst.Common.Parameters import Param, IntParam, StrParam
+from lnst.Common.IpAddress import ipaddress
+from lnst.Controller import HostReq, DeviceReq
+from lnst.Recipes.ENRT.BaseEnrtRecipe import BaseEnrtRecipe, EnrtConfiguration
+from lnst.Devices import VlanDevice
+from lnst.Devices import BondDevice
+
+class VlansOverBondRecipe(BaseEnrtRecipe):
+    m1 = HostReq()
+    m1.eth0 = DeviceReq(label="net1")
+    m1.eth1 = DeviceReq(label="net1")
+
+    m2 = HostReq()
+    m2.eth0 = DeviceReq(label="net1")
+
+    offload_combinations = Param(default=(
+        dict(gro="on", gso="on", tso="on", tx="on"),
+        dict(gro="off", gso="on", tso="on", tx="on"),
+        dict(gro="on", gso="off", tso="off", tx="on"),
+        dict(gro="on", gso="on", tso="off", tx="off")))
+
+    bonding_mode = StrParam(mandatory=True)
+    miimon_value = IntParam(mandatory=True)
+
+    def test_wide_configuration(self):
+        m1, m2 = self.matched.m1, self.matched.m2
+
+        m1.bond = BondDevice(mode=self.params.bonding_mode, miimon=self.params.miimon_value)
+        m1.eth0.down()
+        m1.eth1.down()
+        m1.bond.slave_add(m1.eth0)
+        m1.bond.slave_add(m1.eth1)
+        m1.vlan1 = VlanDevice(realdev=m1.bond, vlan_id=10)
+        m1.vlan2 = VlanDevice(realdev=m1.bond, vlan_id=20)
+
+        m2.vlan1 = VlanDevice(realdev=m2.eth0, vlan_id=10)
+        m2.vlan2 = VlanDevice(realdev=m2.eth0, vlan_id=20)
+
+        #Due to limitations in the current EnrtConfiguration
+        #class, a single vlan test pair is chosen
+        configuration = EnrtConfiguration()
+        configuration.endpoint1 = m1.vlan1
+        configuration.endpoint2 = m2.vlan1
+
+        if "mtu" in self.params:
+            m1.bond.mtu = self.params.mtu
+            m2.eth0.mtu = self.params.mtu
+
+        net_addr_1 = "192.168.10"
+        net_addr_2 = "192.168.20"
+        net_addr6_1 = "fc00:0:0:1"
+        net_addr6_2 = "fc00:0:0:2"
+
+        for i, m in enumerate([m1, m2]):
+            m.vlan1.ip_add(ipaddress(net_addr_1 + "." + str(i+1) + "/24"))
+            m.vlan1.ip_add(ipaddress(net_addr6_1 + "::" + str(i+1) + "/64"))
+            m.vlan2.ip_add(ipaddress(net_addr_2 + "." + str(i+1) + "/24"))
+            m.vlan2.ip_add(ipaddress(net_addr6_2 + "::" + str(i+1) + "/64"))
+
+        m1.eth0.up()
+        m1.eth1.up()
+        m1.bond.up()
+        m1.vlan1.up()
+        m1.vlan2.up()
+        m2.eth0.up()
+        m2.vlan1.up()
+        m2.vlan2.up()
+
+        #TODO better service handling through HostAPI
+        m1.run("service irqbalance stop")
+        m2.run("service irqbalance stop")
+        for m in self.matched:
+            for dev in m.devices:
+                self._pin_dev_interrupts(dev, self.params.dev_intr_cpu)
+
+        return configuration
+
+    def test_wide_deconfiguration(self, config):
+        m1, m2 = self.matched.m1, self.matched.m2
+
+        #TODO better service handling through HostAPI
+        m1.run("service irqbalance start")
+        m2.run("service irqbalance start")
