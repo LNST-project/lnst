d2d3a70e9b2a0608ac9f1c8c1ca98bcc3c295736
Jan Tluka <jtluka@redhat.com>
2020-04-23 16:27:23 +0200

    docs: add ConfigMixins module documentation
    Signed-off-by: Jan Tluka <jtluka@redhat.com>
    Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

--- lnst/Recipes/ENRT/ConfigMixins/__init__.py
+++ lnst/Recipes/ENRT/ConfigMixins/__init__.py
@@ -1,5 +1,8 @@
 """
+This module defines Mixin classes that extend :any:`BaseEnrtRecipe` class.
 
+Each Mixin class provides additional *sub* configuration to the *test_wide*
+configuration of a recipe that inherits from :any:`BaseEnrtRecipe`.
 
 """
 
