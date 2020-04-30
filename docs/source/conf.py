714d73836f9c78401946ffbea83b3c28812fd367
Ondrej Lichtner <olichtne@redhat.com>
2020-04-30 14:05:37 +0200

    docs: fix import path calculation
    Depending on where the 'sphinx-build' is called from, the relative
    import path of '../'+sys.path didn't always work. This fixes the issue
    by defining the path as relative to location of the conf.py file.
    
    Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,15 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+import pathlib
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(pathlib.Path(__file__).parent.absolute(), "../../")
+    ),
+)
 
 
 # -- Project information -----------------------------------------------------
@@ -30,9 +36,6 @@ author = 'Jiri Pirko'
 extensions = [
     'sphinx.ext.autodoc',
 ]
-
-import sys
-sys.path = ['../'] + sys.path
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
