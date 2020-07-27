57b0c910ba91bc4fa55027be443e20c09b05d5b9
Jan Tluka <jtluka@redhat.com>
2020-07-27 17:51:34 +0200

    Recipes.ENRT.PerfTestMixins: add description of SctpFirewallPerfTest mixin
    Signed-off-by: Jan Tluka <jtluka@redhat.com>
    Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

--- lnst/Recipes/ENRT/PerfTestMixins/BasePerfTestTweakMixin.py
+++ lnst/Recipes/ENRT/PerfTestMixins/BasePerfTestTweakMixin.py
@@ -4,8 +4,12 @@ class BasePerfTestTweakMixin(object):
     mixin classes.
     """
 
+    def generate_perf_test_tweak_description(self, perf_config):
+        return ["Performance test tweaks:"]
+
     def apply_perf_test_tweak(self, perf_config):
-        pass
+        perf_config.perf_test_tweak_config = {}
 
     def remove_perf_test_tweak(self, perf_config):
+        # TODO: check if anything left in the perf_config.perf_test_tweak_config
         pass
--- lnst/Recipes/ENRT/PerfTestMixins/SctpFirewallPerfTestMixin.py
+++ lnst/Recipes/ENRT/PerfTestMixins/SctpFirewallPerfTestMixin.py
@@ -20,6 +20,9 @@ class SctpFirewallPerfTestMixin(BasePerfTestTweakMixin):
                     job_level=ResultLevel.NORMAL,
                 )
 
+            tweak_config = perf_config.perf_test_tweak_config
+            tweak_config["iptables_sctp"] = True
+
     def remove_perf_test_tweak(self, perf_config):
         flow_measurement = self._get_flow_measurement_from_config(perf_config)
         flow = flow_measurement.conf[0]
@@ -29,5 +32,23 @@ class SctpFirewallPerfTestMixin(BasePerfTestTweakMixin):
                     "iptables -D OUTPUT ! -o %s -p sctp -j DROP" % nic.name,
                     job_level=ResultLevel.NORMAL,
                 )
+            tweak_config = perf_config.perf_test_tweak_config
+            del tweak_config["iptables_sctp"]
 
         super().remove_perf_test_tweak(perf_config)
+
+    def generate_perf_test_tweak_description(self, perf_config):
+        description = super().generate_perf_test_tweak_description(perf_config)
+        tweak_config = perf_config.perf_test_tweak_config
+        if "iptables_sctp" in tweak_config:
+            description.append(
+                "added iptables rules to drop SCTP packets on other than "
+                "tested interface"
+                )
+        else:
+            description.append(
+                "skipped addition of iptables rules to drop SCTP packets on "
+                "other than tested interface"
+                )
+
+        return description
