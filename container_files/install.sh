74814999c359e68ccca1f8547324ac93cb39d138
Samuel Dobron <sdobron@redhat.com>
2021-10-01 15:28:19 +0200

    containerized lnst-slave
    Added `Dockerfile` and `install.sh`, these files implements containerization of lnst-slave.

---
+++ container_files/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+poetry install
+
+venv_path=$(poetry env info -p)
+echo "$venv_path"
+ln -s $venv_path/bin /lnst/.bin
+
+echo "================== Installing iperf  ======================"
+cd /
+git clone https://github.com/LNST-project/iperf.git
+cd iperf
+git checkout mptcp_udp_retry_cherrypick
+./configure && make && make install
+echo "================== Successfully installed iperf  ======================"
+
+echo "================== Installing neper  ======================"
+git clone -b lnst-production https://github.com/LNST-project/neper.git
+cd neper
+git --no-pager show --summary HEAD
+make -s
+echo "================== Successfully installed neper  ======================"
+echo "================== Successfully installed LNST  ======================"
+
