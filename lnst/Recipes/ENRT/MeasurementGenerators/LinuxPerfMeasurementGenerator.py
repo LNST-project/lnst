9429a252b6bd9322091465698f0d0054db357e0c
Jan Tluka <jtluka@redhat.com>
2022-10-12 13:13:29 +0200

    LinuxPerfMeasurementGenerator: add linuxperf_cpus_override parameter
    This is to override the linuxperf_cpus property defined by a recipe that
    uses the LinuxPerfMeasurementGenerator extension.
    
    In following example two groups of cpus will be profiled separately:
    
    linuxperf_cpus_override=[ [0,1], [2,3] ]
    
    This patch explicitly disallows use of empty list of cpus to be
    profiled. In that case, an exception is raised.
    
    Possibility to profile the whole system (an empty list of cpus) will be
    added later in future.
    
    Signed-off-by: Jan Tluka <jtluka@redhat.com>

--- lnst/Recipes/ENRT/MeasurementGenerators/LinuxPerfMeasurementGenerator.py
+++ lnst/Recipes/ENRT/MeasurementGenerators/LinuxPerfMeasurementGenerator.py
@@ -7,12 +7,19 @@ from lnst.RecipeCommon.Perf.Measurements.BaseMeasurement import (
 from lnst.RecipeCommon.Perf.Measurements.LinuxPerfMeasurement import (
     LinuxPerfMeasurement,
 )
-from lnst.Common.Parameters import BoolParam
+from lnst.Common.Parameters import (
+    BoolParam,
+    IntParam,
+    ListParam,
+)
+from lnst.Common.LnstError import LnstError
 
 import os
 
+
 class LinuxPerfMeasurementGenerator(BaseMeasurementGenerator):
     do_linuxperf_measurement = BoolParam(default=False)
+    linuxperf_cpus_override = ListParam(type=ListParam(type=IntParam()), mandatory=False)
 
     def generate_perf_measurements_combinations(self, config):
         combinations = super().generate_perf_measurements_combinations(config)
@@ -27,12 +34,27 @@ class LinuxPerfMeasurementGenerator(BaseMeasurementGenerator):
             except FileExistsError:
                 pass
 
+        profiled_cpu_groups: list[list[int]] = []
+        if self.params.get("linuxperf_cpus_override"):
+            profiled_cpu_groups = self.params.linuxperf_cpus_override
+        else:
+            profiled_cpu_groups = getattr(self, "linuxperf_cpus", [])
+
+        # TODO: in case no group of cpus is in the list, we may simply run
+        # profiler without the cpu specified, however this requires additional
+        # changes in the LinuxPerfMeasurement class; for now, let's just
+        # disallow this by raising an exception
+        if self.params.do_linuxperf_measurement and not profiled_cpu_groups:
+            raise LnstError(
+                "Cannot profile empty list of cpus when do_linuxperf_measurement parameter is specified"
+            )
+
         for combination in combinations:
             res: list[BaseMeasurement]
             if self.params.do_linuxperf_measurement:
                 measurement: BaseMeasurement = LinuxPerfMeasurement(
                     self.matched,
-                    self.linuxperf_cpus,
+                    profiled_cpu_groups,
                     data_folder=linuxperf_data_folder,
                     recipe_conf=config,
                 )
