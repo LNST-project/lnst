5ddf480d4bf84f804935054bb86ee8901ec43440
Jozef Urbanovsky <jurbanov@redhat.com>
2022-11-29 14:21:40 +0100

    Controller.Machine: add get_dev_by_ifname method
    This method allows us to locate newly created VF
    in any namespace network, specified by parameter

--- lnst/Controller/Machine.py
+++ lnst/Controller/Machine.py
@@ -279,6 +279,13 @@ class Machine(object):
                 return dev
         return None
 
+    def get_dev_by_ifname(self, ifname, netns=None):
+        ns_instance = self._get_netns_by_name(netns)
+        for dev in self._device_database[ns_instance].values():
+            if dev.name == ifname:
+                return dev
+        return None
+
     def _find_device_in_any_namespace(self, ifindex, peer_ifindex=None):
         for ns in list(self._namespaces.keys()) + [None]:
             dev = self.dev_db_get_ifindex(ifindex, ns)
