49e8297628804774f6b967256e60d6e1eaa1f3ff
Jan Tluka <jtluka@redhat.com>
2023-01-11 13:08:55 +0100

    recipes/GreTunnelOverVlanRecipe.py: add ip address parameter
    Signed-off-by: Jan Tluka <jtluka@redhat.com>

--- lnst/Recipes/ENRT/GreTunnelOverVlanRecipe.py
+++ lnst/Recipes/ENRT/GreTunnelOverVlanRecipe.py
@@ -1,11 +1,15 @@
 from lnst.Controller import HostReq, DeviceReq, RecipeParam
 from lnst.Common.IpAddress import (
     AF_INET,
-    ipaddress,
     Ip4Address,
     Ip6Address,
+    interface_addresses,
 )
-from lnst.Common.Parameters import Param, IntParam
+from lnst.Common.Parameters import (
+    Param,
+    IntParam,
+    IPv4NetworkParam,
+)
 from lnst.Devices import GreDevice, VlanDevice
 from lnst.RecipeCommon.Ping.PingEndpoints import PingEndpoints
 from lnst.RecipeCommon.PacketAssert import PacketAssertConf
@@ -74,6 +78,8 @@ class GreTunnelOverVlanRecipe(
         )
     )
 
+    net_ipv4 = IPv4NetworkParam(default="192.168.101.0/24")
+
     def configure_underlying_network(self, configuration):
         """
         The underlying network for the tunnel consists of two Ethernet
@@ -85,8 +91,9 @@ class GreTunnelOverVlanRecipe(
         host1.vlan0 = VlanDevice(realdev=host1.eth0, vlan_id=self.params.vlan_id)
         host2.vlan0 = VlanDevice(realdev=host2.eth0, vlan_id=self.params.vlan_id)
 
-        for i, device in enumerate([host1.vlan0, host2.vlan0]):
-            device.ip_add(ipaddress("192.168.101." + str(i + 1) + "/24"))
+        ipv4_addr = interface_addresses(self.params.net_ipv4)
+        for device in [host1.vlan0, host2.vlan0]:
+            device.ip_add(next(ipv4_addr))
             configuration.test_wide_devices.append(device)
 
         for dev in [
