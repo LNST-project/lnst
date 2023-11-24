f014c4308c9d269b08d15c46b3545ae337694727
Ondrej Lichtner <olichtne@redhat.com>
2023-11-24 16:36:27 +0100

    BaselineEvaluator: refactor to unify baseline evaluators
    Now that MeasurementResults export a `metrics` property, Baseline
    evaluation can be refactored and unified to work with almost any
    MeasurementResult in a much simpler manner.
    
    This commit does just that, additionally there are two additional
    related changes:
    * MetricComparison class is extended to contain more information so that
      we can unify it and remove the use of a "comparison dictionary"
    * BaselineEvaluationResult class is added to store all of the grouped
      MetricComparisons and to generate a nice description for them.
    
    This does change the Result description format as it was previously more
    customized to the individual MeasurementResults, but this IMO actually
    is more accurate although maybe a little less nice to look at.
    
    Finally this gives us a bunch more power to actually machine post
    process the baseline evaluations so I've also removed the "good
    direction -> improvement warning" override which IMO should be done as a
    post processing step and not directly as a part of the evaluation
    method.
    
    The unification of the BaselineEvaluator means that we can completely
    remove:
    * BaselineFlowAverageEvaluator
    * BaselineRDMABandwidthAverageEvaluator
    * BaselineTcRunAverageEvaluator
    
    We need to keep the BaselineCPUAverageEvaluator as this does still
    override some methods wrt. how filtering and result grouping works for
    CPU Measurements.
    
    Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

--- lnst/RecipeCommon/Perf/Evaluators/BaselineEvaluator.py
+++ lnst/RecipeCommon/Perf/Evaluators/BaselineEvaluator.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from functools import reduce
 from dataclasses import dataclass
 
@@ -6,23 +6,58 @@ from lnst.Controller.Recipe import BaseRecipe
 from lnst.Controller.RecipeResults import ResultType, Result
 from lnst.RecipeCommon.BaseResultEvaluator import BaseResultEvaluator
 from lnst.RecipeCommon.Perf.Recipe import RecipeConf as PerfRecipeConf
+from lnst.RecipeCommon.Perf.Results import result_averages_difference
 from lnst.RecipeCommon.Perf.Measurements.Results import (
     BaseMeasurementResults as PerfMeasurementResults,
 )
 
 
-class BaselineEvaluationResult(Result):
-    pass
-
-
 @dataclass
 class MetricComparison:
+    measurement_type: str
+    current_result: PerfMeasurementResults
+    baseline_result: Optional[PerfMeasurementResults]
+    threshold: float
     metric_name: str
-    result: ResultType
+    difference: float
+    comparison_result: ResultType
     text: str
 
 
+class BaselineEvaluationResult(Result):
+    def __init__(
+        self, comparisons: list[MetricComparison], recipe_conf: PerfRecipeConf
+    ):
+        super().__init__(ResultType.PASS)
+        self.comparisons = comparisons
+        self.recipe_conf = recipe_conf
+
+    @property
+    def result(self) -> ResultType:
+        return reduce(
+            ResultType.max_severity,
+            [comparison.comparison_result for comparison in self.comparisons],
+            ResultType.PASS,
+        )
+
+    @property
+    def description(self) -> str:
+        res = []
+        current_result = None
+        for comparison in self.comparisons:
+            if comparison.current_result != current_result:
+                res.append(comparison.current_result.describe())
+                current_result = comparison.current_result
+            res.append(f"{comparison.comparison_result}: {comparison.text}")
+        return "\n".join(
+            ["Baseline evaluation of"] + res
+        )
+
+
 class BaselineEvaluator(BaseResultEvaluator):
+    def __init__(self, metrics_to_evaluate: Optional[List[str]] = None):
+        self._metrics_to_evaluate = metrics_to_evaluate
+
     def evaluate_results(
         self,
         recipe: BaseRecipe,
@@ -59,52 +94,21 @@ class BaselineEvaluator(BaseResultEvaluator):
     ):
         cumulative_result = ResultType.PASS
         comparisons = []
-        result_text = self.describe_group_results(recipe, recipe_conf, results)
 
         baselines = self.get_baselines(recipe, recipe_conf, results)
-        result_index = len(recipe.current_run.results)
-        for i, (result, baseline) in enumerate(zip(results, baselines)):
-            metric_comparisons = self.compare_result_with_baseline(
-                recipe, recipe_conf, result, baseline, result_index
-            )
-            cumulative_result = reduce(
-                ResultType.max_severity,
-                [metric.result for metric in metric_comparisons],
-                cumulative_result,
-            )
-            result_text.extend(
-                [metric.text for metric in metric_comparisons]
-            )
+        for result, baseline in zip(results, baselines):
             comparisons.extend(
-                [
-                    {
-                        "measurement_type": result.measurement.__class__.__name__,
-                        "current_result": result,
-                        "baseline_result": baseline,
-                        "comparison_result": metric.result,
-                        "metric_name": metric.metric_name,
-                        "text": metric.text,
-                        "recipe_conf": recipe_conf,
-                    }
-                    for metric in metric_comparisons
-                ]
+                self.compare_result_with_baseline(
+                    recipe, recipe_conf, result, baseline
+                )
             )
 
         recipe.add_custom_result(
             BaselineEvaluationResult(
-                cumulative_result,
-                "\n".join(result_text),
-                data={"comparisons": comparisons},
+                comparisons=comparisons,
+                recipe_conf=recipe_conf,
             )
         )
-
-    def describe_group_results(
-        self,
-        recipe: BaseRecipe,
-        recipe_conf: PerfRecipeConf,
-        results: List[PerfMeasurementResults],
-    ) -> List[str]:
-        return []
 
     def get_baselines(
         self,
@@ -121,7 +125,14 @@ class BaselineEvaluator(BaseResultEvaluator):
         recipe: BaseRecipe,
         recipe_conf: PerfRecipeConf,
         result: PerfMeasurementResults,
-    ) -> PerfMeasurementResults:
+    ) -> Optional[PerfMeasurementResults]:
+        return None
+
+    def get_threshold(
+        self,
+        baseline: PerfMeasurementResults,
+        metric_name: str,
+    ) -> Optional[float]:
         return None
 
     def compare_result_with_baseline(
@@ -130,6 +141,58 @@ class BaselineEvaluator(BaseResultEvaluator):
         recipe_conf: PerfRecipeConf,
         result: PerfMeasurementResults,
         baseline: PerfMeasurementResults,
-        result_index: int = 0,
     ) -> List[MetricComparison]:
-        raise NotImplementedError("Result to baseline metric comparison not implemented")
+        comparisons = []
+
+        if self._metrics_to_evaluate:
+            metrics_to_evaluate = [
+                i for i in result.metrics if i in self._metrics_to_evaluate
+            ]
+        else:
+            metrics_to_evaluate = result.metrics
+
+        for metric in metrics_to_evaluate:
+            comparisons.append(
+                self.compare_metrics_with_threshold(
+                    result=result,
+                    baseline=baseline,
+                    metric_name=metric,
+                )
+            )
+        return comparisons
+
+    def compare_metrics_with_threshold(self, result, baseline, metric_name):
+        threshold = None
+        diff = None
+
+        if not baseline:
+            comparison_result = ResultType.FAIL
+            text = "No baseline found"
+        elif (threshold := self.get_threshold(baseline, metric_name)) is None:
+            comparison_result = ResultType.FAIL
+            text = "No threshold found"
+        else:
+            diff = result_averages_difference(
+                getattr(result, metric_name),
+                getattr(baseline, metric_name),
+            )
+            direction = "higher" if diff >= 0 else "lower"
+
+            comparison_result = (
+                ResultType.PASS if abs(diff) <= threshold else ResultType.FAIL
+            )
+            text = (
+                f"New {metric_name} average is {abs(diff):.2f}% {direction} from the baseline. "
+                f"Allowed difference: {threshold}%"
+            )
+
+        return MetricComparison(
+            measurement_type=result.measurement.__class__.__name__,
+            current_result=result,
+            baseline_result=baseline,
+            threshold=threshold,
+            metric_name=metric_name,
+            difference=diff,
+            comparison_result=comparison_result,
+            text=text,
+        )
