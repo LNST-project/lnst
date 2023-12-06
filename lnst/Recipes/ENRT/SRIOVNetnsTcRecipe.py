49f0e7f5a4d563296a658604c2274955fcf22d33
Jan Tluka <jtluka@redhat.com>
2023-12-06 16:16:21 +0100

    Recipes/ENRT: use SRIOVDevices in BaseSRIOVNetnsTcRecipe
    Update of the recipe to use the SRIOVDevices for configuration of a
    switchdev capable device.
    
    Signed-off-by: Jan Tluka <jtluka@redhat.com>

--- lnst/Recipes/ENRT/SRIOVNetnsTcRecipe.py
+++ lnst/Recipes/ENRT/SRIOVNetnsTcRecipe.py
@@ -37,102 +37,105 @@ class SRIOVNetnsTcRecipe(BaseSRIOVNetnsTcRecipe):
 
         config.ingress_devices = []
         for host in [host1, host2]:
-            host.run(f"tc qdisc add dev {host.vf_representor_eth0.name} ingress")
+            host.run(f"tc qdisc add dev {host.sriov_devices.vf_reps[0].name} ingress")
             host.run(f"tc qdisc add dev {host.eth0.name} ingress")
-            config.ingress_devices.extend([host.vf_representor_eth0, host.eth0])
+            config.ingress_devices.extend([host.sriov_devices.vf_reps[0], host.eth0])
+
+        host1_vf_dev, host1_vf_rep_dev = host1.sriov_devices[0]
+        host2_vf_dev, host2_vf_rep_dev = host2.sriov_devices[0]
 
         host1.run(f"tc filter add dev {host1.eth0.name} "
                   f"protocol ip ingress flower skip_sw "
-                  f"src_mac {host2.newns.vf_eth0.hwaddr} "
-                  f"dst_mac {host1.newns.vf_eth0.hwaddr} "
-                  f"action mirred egress redirect dev {host1.vf_representor_eth0.name}")
+                  f"src_mac {host2_vf_dev.hwaddr} "
+                  f"dst_mac {host1_vf_dev.hwaddr} "
+                  f"action mirred egress redirect dev {host1_vf_rep_dev.name}")
 
         host1.run(f"tc filter add dev {host1.eth0.name} "
                   f"protocol arp ingress flower "
-                  f"src_mac {host2.newns.vf_eth0.hwaddr} "
-                  f"dst_mac {host1.newns.vf_eth0.hwaddr} "
-                  f"action mirred egress redirect dev {host1.vf_representor_eth0.name}")
+                  f"src_mac {host2_vf_dev.hwaddr} "
+                  f"dst_mac {host1_vf_dev.hwaddr} "
+                  f"action mirred egress redirect dev {host1_vf_rep_dev.name}")
 
         host1.run(f"tc filter add dev {host1.eth0.name} "
                   f"protocol arp ingress flower "
-                  f"src_mac {host2.newns.vf_eth0.hwaddr} "
+                  f"src_mac {host2_vf_dev.hwaddr} "
                   f"dst_mac FF:FF:FF:FF:FF:FF "
-                  f"action mirred egress redirect dev {host1.vf_representor_eth0.name}")
+                  f"action mirred egress redirect dev {host1_vf_rep_dev.name}")
 
-        host1.run(f"tc filter add dev {host1.vf_representor_eth0.name} "
+        host1.run(f"tc filter add dev {host1_vf_rep_dev.name} "
                   f"protocol ip ingress flower skip_sw "
-                  f"src_mac {host1.newns.vf_eth0.hwaddr} "
-                  f"dst_mac {host2.newns.vf_eth0.hwaddr} "
+                  f"src_mac {host1_vf_dev.hwaddr} "
+                  f"dst_mac {host2_vf_dev.hwaddr} "
                   f"action mirred egress redirect dev {host1.eth0.name}")
 
-        host1.run(f"tc filter add dev {host1.vf_representor_eth0.name} "
+        host1.run(f"tc filter add dev {host1_vf_rep_dev.name} "
                   f"protocol arp ingress flower "
-                  f"src_mac {host1.newns.vf_eth0.hwaddr} "
-                  f"dst_mac {host2.newns.vf_eth0.hwaddr} "
+                  f"src_mac {host1_vf_dev.hwaddr} "
+                  f"dst_mac {host2_vf_dev.hwaddr} "
                   f"action mirred egress redirect dev {host1.eth0.name}")
 
-        host1.run(f"tc filter add dev {host1.vf_representor_eth0.name} "
+        host1.run(f"tc filter add dev {host1_vf_rep_dev.name} "
                   f"protocol arp ingress flower "
-                  f"src_mac {host1.newns.vf_eth0.hwaddr} "
+                  f"src_mac {host1_vf_dev.hwaddr} "
                   f"dst_mac FF:FF:FF:FF:FF:FF "
                   f"action mirred egress redirect dev {host1.eth0.name}")
 
         host2.run(f"tc filter add dev {host2.eth0.name} "
                   f"protocol ip ingress flower skip_sw "
-                  f"src_mac {host1.newns.vf_eth0.hwaddr} "
-                  f"dst_mac {host2.newns.vf_eth0.hwaddr} "
-                  f"action mirred egress redirect dev {host2.vf_representor_eth0.name}")
+                  f"src_mac {host1_vf_dev.hwaddr} "
+                  f"dst_mac {host2_vf_dev.hwaddr} "
+                  f"action mirred egress redirect dev {host2_vf_rep_dev.name}")
 
         host2.run(f"tc filter add dev {host2.eth0.name} "
                   f"protocol arp ingress flower "
-                  f"src_mac {host1.newns.vf_eth0.hwaddr} "
-                  f"dst_mac {host2.newns.vf_eth0.hwaddr} "
-                  f"action mirred egress redirect dev {host2.vf_representor_eth0.name}")
+                  f"src_mac {host1_vf_dev.hwaddr} "
+                  f"dst_mac {host2_vf_dev.hwaddr} "
+                  f"action mirred egress redirect dev {host2_vf_rep_dev.name}")
 
         host2.run(f"tc filter add dev {host2.eth0.name} "
                   f"protocol arp ingress flower "
-                  f"src_mac {host1.newns.vf_eth0.hwaddr} "
+                  f"src_mac {host1_vf_dev.hwaddr} "
                   f"dst_mac FF:FF:FF:FF:FF:FF "
-                  f"action mirred egress redirect dev {host2.vf_representor_eth0.name}")
+                  f"action mirred egress redirect dev {host2_vf_rep_dev.name}")
 
-        host2.run(f"tc filter add dev {host2.vf_representor_eth0.name} "
+        host2.run(f"tc filter add dev {host2_vf_rep_dev.name} "
                   f"protocol ip ingress flower skip_sw "
-                  f"src_mac {host2.newns.vf_eth0.hwaddr} "
-                  f"dst_mac {host1.newns.vf_eth0.hwaddr} "
+                  f"src_mac {host2_vf_dev.hwaddr} "
+                  f"dst_mac {host1_vf_dev.hwaddr} "
                   f"action mirred egress redirect dev {host2.eth0.name}")
 
-        host2.run(f"tc filter add dev {host2.vf_representor_eth0.name} "
+        host2.run(f"tc filter add dev {host2_vf_rep_dev.name} "
                   f"protocol arp ingress flower "
-                  f"src_mac {host2.newns.vf_eth0.hwaddr} "
-                  f"dst_mac {host1.newns.vf_eth0.hwaddr} "
+                  f"src_mac {host2_vf_dev.hwaddr} "
+                  f"dst_mac {host1_vf_dev.hwaddr} "
                   f"action mirred egress redirect dev {host2.eth0.name}")
 
-        host2.run(f"tc filter add dev {host2.vf_representor_eth0.name} "
+        host2.run(f"tc filter add dev {host2_vf_rep_dev.name} "
                   f"protocol arp ingress flower "
-                  f"src_mac {host2.newns.vf_eth0.hwaddr} "
+                  f"src_mac {host2_vf_dev.hwaddr} "
                   f"dst_mac FF:FF:FF:FF:FF:FF "
                   f"action mirred egress redirect dev {host2.eth0.name}")
 
     @property
     def pause_frames_dev_list(self):
-        return [self.matched.host1.newns.vf_eth0, self.matched.host2.newns.vf_eth0]
+        return [self.matched.host1.sriov_devices.vfs[0], self.matched.host2.sriov_devices.vfs[0]]
 
     @property
     def offload_nics(self):
-        return [self.matched.host1.newns.vf_eth0, self.matched.host2.newns.vf_eth0]
+        return [self.matched.host1.sriov_devices.vfs[0], self.matched.host2.sriov_devices.vfs[0]]
 
     @property
     def mtu_hw_config_dev_list(self):
-        return [self.matched.host1.newns.vf_eth0, self.matched.host2.newns.vf_eth0]
+        return [self.matched.host1.sriov_devices.vfs[0], self.matched.host2.sriov_devices.vfs[0]]
 
     @property
     def coalescing_hw_config_dev_list(self):
-        return [self.matched.host1.newns.vf_eth0, self.matched.host2.newns.vf_eth0]
+        return [self.matched.host1.sriov_devices.vfs[0], self.matched.host2.sriov_devices.vfs[0]]
 
     @property
     def dev_interrupt_hw_config_dev_list(self):
-        return [self.matched.host1.newns.vf_eth0, self.matched.host2.newns.vf_eth0]
+        return [self.matched.host1.sriov_devices.vfs[0], self.matched.host2.sriov_devices.vfs[0]]
 
     @property
     def parallel_stream_qdisc_hw_config_dev_list(self):
-        return [self.matched.host1.newns.vf_eth0, self.matched.host2.newns.vf_eth0]
+        return [self.matched.host1.sriov_devices.vfs[0], self.matched.host2.sriov_devices.vfs[0]]
