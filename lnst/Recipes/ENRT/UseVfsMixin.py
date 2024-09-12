1b566c65adbaca2065c62f15ac61dd77293b8fc0
Jan Tluka <jtluka@redhat.com>
2024-09-12 16:19:46 +0200

    Recipes/ENRT/UseVfsMixin.py: add vf_trust parameter
    Allows user to set trust mode on created VFs.
    
    Signed-off-by: Jan Tluka <jtluka@redhat.com>

--- lnst/Recipes/ENRT/UseVfsMixin.py
+++ lnst/Recipes/ENRT/UseVfsMixin.py
@@ -1,6 +1,7 @@
-from lnst.Common.Parameters import BoolParam
+from lnst.Common.Parameters import BoolParam, ChoiceParam
 from lnst.Controller.Requirements import DeviceReq
 from lnst.Recipes.ENRT.SRIOVDevices import SRIOVDevices
+from lnst.Devices import RemoteDevice
 
 
 class UseVfsMixin:
@@ -12,11 +13,16 @@ class UseVfsMixin:
     with VF Device instances. This allows user to interact with the network
     interfaces without additional changes to the code of recipe.
 
+    Mixin provides two parameters:
+    * use_vfs - main boolean parameter to enable or disable (default) use of VFs
+    * vf_trust - (optional) set the trust parameter of the used VFs, 'on' or 'off'
+
     There are some limitations, for example pause frames cannot be configured
     since the VF do not support these.
     """
 
     use_vfs = BoolParam(default=False)
+    vf_trust = ChoiceParam(choices={'on', 'off'})
 
     def test_wide_configuration(self):
         config = super().test_wide_configuration()
@@ -24,7 +30,7 @@ class UseVfsMixin:
         if not self.params.use_vfs:
             return config
 
-        config.vf_config = {}
+        self.vf_config = {}
         for host_key, host_req in self.req:
             dev_names = [key for key, value in host_req if isinstance(value, DeviceReq)]
             host = getattr(self.matched, host_key)
@@ -33,17 +39,22 @@ class UseVfsMixin:
             for dev_name in dev_names:
                 dev = getattr(host, dev_name)
                 sriov_devices = SRIOVDevices(dev, 1)
+
                 vf_dev = sriov_devices.vfs[0]
                 host.map_device(dev_name, {"ifname": vf_dev.name})
 
-                host_config = config.vf_config.setdefault(host, [])
+                host_config = self.vf_config.setdefault(host, [])
                 host_config.append(sriov_devices)
+
+        if self.params.get("vf_trust"):
+            for dev in self.vf_trust_device_list:
+                dev.vf_trust(0, self.params.vf_trust)
 
         return config
 
     def test_wide_deconfiguration(self, config):
         if self.params.use_vfs:
-            for host, sriov_devices_list in config.vf_config.items():
+            for host, sriov_devices_list in self.vf_config.items():
                 for sriov_devices in sriov_devices_list:
                     vf_dev = sriov_devices.vfs[0]
                     host.map_device(vf_dev._id, {"ifname": sriov_devices.phys_dev.name})
@@ -56,10 +67,14 @@ class UseVfsMixin:
 
         if self.params.use_vfs:
             description += [
-                f"Using vf device {vf_dev.name} of pf {sriov_devices.phys_dev.name} for DeviceReq {host.hostid}.{vf_dev._id}"
-                for host, sriov_devices_list in config.vf_config.items()
+                f"Using vf device {vf_dev.name} of pf {sriov_devices.phys_dev.name} for DeviceReq {host.hostid}.{vf_dev._id}" + (f" trusted={self.params.vf_trust}" if self.params.get("vf_trust") and sriov_devices.phys_dev in self.vf_trust_device_list else "")
+                for host, sriov_devices_list in self.vf_config.items()
                 for sriov_devices in sriov_devices_list
                 for vf_dev in sriov_devices.vfs
             ]
 
         return description
+
+    @property
+    def vf_trust_device_list(self) -> list[RemoteDevice]:
+        return []
