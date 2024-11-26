012921b18b54f3d943534580e5b9342f619946e1
Ondrej Lichtner <olichtne@redhat.com>
2024-11-26 16:13:21 +0100

    AggregatedXDPBenchMeasurementResults: add individual_results
    For some reason this Aggregated class was missing the individual_results
    property.
    
    Signed-off-by: Ondrej Lichtner <olichtne@redhat.com>

--- lnst/RecipeCommon/Perf/Measurements/Results/AggregatedXDPBenchMeasurementResults.py
+++ lnst/RecipeCommon/Perf/Measurements/Results/AggregatedXDPBenchMeasurementResults.py
@@ -8,8 +8,13 @@ from lnst.RecipeCommon.Perf.Results import SequentialPerfResult
 class AggregatedXDPBenchMeasurementResults(XDPBenchMeasurementResults):
     def __init__(self, measurement, flow):
         super().__init__(measurement, True, flow)
+        self._individual_results: list[TcRunMeasurementResults] = []
         self._generator_results = SequentialPerfResult()
         self._receiver_results = SequentialPerfResult()
+
+    @property
+    def individual_results(self) -> list[XDPBenchMeasurementResults]:
+        return self._individual_results
 
     @property
     def measurement_success(self) -> bool:
@@ -22,9 +27,11 @@ class AggregatedXDPBenchMeasurementResults(XDPBenchMeasurementResults):
         if results is None:
             return
         elif isinstance(results, AggregatedXDPBenchMeasurementResults):
+            self._individual_results.extend(results.individual_results)
             self.generator_results.extend(results.generator_results)
             self.receiver_results.extend(results.receiver_results)
         elif isinstance(results, XDPBenchMeasurementResults):
+            self._individual_results.append(results)
             self.generator_results.append(results.generator_results)
             self.receiver_results.append(results.receiver_results)
         else:
