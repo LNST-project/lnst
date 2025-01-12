592ee7f378192a7b714f6d4ffdbfef8befe24b08
Samuel Dobron <sdobron@redhat.com>
2025-01-12 23:47:50 +0100

    Recipes: added ForwardingRecipe
    Added ForwardingRecipe that measures performance
    of whole forwarding stack. This can be extended in
    a way it'll test specific devices/other forwarding
    planes.

---
+++ lnst/RecipeCommon/Perf/Measurements/Results/AggregatedForwardingMeasurementResults.py
@@ -0,0 +1,55 @@
+"""
+Module with implementing results container for aggregated forwarding recipes
+results.
+
+Copyright 2025 Red Hat, Inc.
+Licensed under the GNU General Public License, version 2 as
+published by the Free Software Foundation; see COPYING for details.
+"""
+
+__author__ = """
+sdobron@redhat.com (Samuel Dobron)
+"""
+
+
+from lnst.RecipeCommon.Perf.Results import SequentialPerfResult
+from .ForwardingMeasurementResults import ForwardingMeasurementResults
+from lnst.RecipeCommon.Perf.Measurements.MeasurementError import MeasurementError
+
+
+class AggregatedForwardingMeasurementResults(ForwardingMeasurementResults):
+    def __init__(self, measurement, flow):
+        super().__init__(measurement, True, flow)
+        self._flow = flow
+        self._generator_results = SequentialPerfResult()
+        self._generator_cpu_stats = SequentialPerfResult()
+        self._receiver_results = SequentialPerfResult()
+        self._receiver_cpu_stats = SequentialPerfResult()
+        self._individual_results: list[ForwardingMeasurementResults] = []
+
+    @property
+    def individual_results(self) -> list[ForwardingMeasurementResults]:
+        return self._individual_results
+
+    @property
+    def measurement_success(self) -> bool:
+        if self.individual_results:
+            return all(res.measurement_success for res in self.individual_results)
+        else:
+            return False
+
+    def add_results(self, results):
+        if results is None:
+            return
+        elif isinstance(results, AggregatedForwardingMeasurementResults):
+            self._individual_results.extend(results.individual_results)
+            self.generator_results.extend(results.generator_results)
+            self.receiver_results.extend(results.receiver_results)
+            self.forwarder_results.extend(results.forwarder_results)
+        elif isinstance(results, ForwardingMeasurementResults):
+            self._individual_results.append(results)
+            self.generator_results.append(results.generator_results)
+            self.receiver_results.append(results.receiver_results)
+            self.forwarder_results.append(results.forwarder_results)
+        else:
+            raise MeasurementError("Adding incorrect results.")
