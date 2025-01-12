cb82a8c3a90363ec8e0216cb3bb593d41082ba3d
Samuel Dobron <sdobron@redhat.com>
2025-01-12 23:39:32 +0100

    Tests/PktGen: rework
    Reworked pktgen implementation to better reflect it's structure.
    Now, instead of all-in-one, we have separated devices, threads
    and controller.
    Device represents pktgen's device (cpu+interface), Thread
    represents separate CPU where pktgen will be running and
    Controller is system-wide pktgen controller.
    
    Apart from rework:
      * added custom flags support (passed in `flags` list)
      * added vlans support (just set `vlan_id`)
      * added `ratep` support for specifying pps
      * added support for setting src/dst ports
      * allow PktGen job to be interrupted by `SIGINT`
        * measurement won't crash when `SIGINT` is received, so now we can
          run pktgen until interrupted (`duration` needs to be set to `0`).

--- lnst/Tests/PktGen.py
+++ lnst/Tests/PktGen.py
@@ -1,6 +1,7 @@
 import re
 import time
 import logging
+from dataclasses import dataclass, field
 from subprocess import Popen, check_output, CalledProcessError
 from threading import Thread
 from typing import Iterator, Union
@@ -8,7 +9,14 @@ from typing import Iterator, Union
 from lnst.Common.Utils import kmod_loaded
 from lnst.Common.IpAddress import Ip4Address
 from lnst.Tests.BaseTestModule import BaseTestModule, TestModuleError
-from lnst.Common.Parameters import IntParam, IpParam, StrParam, ListParam, DeviceParam
+from lnst.Common.Parameters import (
+    IntParam,
+    IpParam,
+    StrParam,
+    ListParam,
+    DeviceParam,
+)
+from lnst.Devices.Device import Device
 
 
 class PktGenResultsSampler:
@@ -107,57 +115,209 @@ class PktGenResultsSampler:
         return values
 
 
-class PktGen(BaseTestModule):
+@dataclass
+class PktgenDevice:
     """
-    In the scope of this module, the physical interface is refered as `interface`.
-    Pktgen device (interface@anything) is refered as device.
+    Class representing a single pktgen device. Each device is tied to
+    a PktgenThread and so, a separate CPU.
 
-    Inspired by https://github.com/torvalds/linux/blob/master/samples/pktgen/pktgen_sample03_burst_single_flow.sh
+    Running multiple PktgenDevices with the same src/dst IPs is almost
+    the same as running iperf --parallel.
+
+    Each device can generate packets for single flow only, so if you
+    want to generate multiple flows in parallel, you need to create
+    multiple pktgen devices (which is just tuple inf+cpu). There
+    might be multiple pktgen devices generating packets for the same
+    interface, but they need to be pinned to separate CPUs.
     """
 
-    cpus = ListParam(type=IntParam())  # each CPU is 1 generator
+    cpu: IntParam  # each CPU is 1 generator
 
-    src_if = DeviceParam()
-    dst_mac = StrParam()
+    src_if: DeviceParam
+    dst_mac: StrParam
 
-    src_ip = IpParam()
-    dst_ip = IpParam()
+    src_ip: IpParam
+    dst_ip: IpParam
 
-    count = IntParam(default=0)  # 0 = no upper limit
-    pkt_size = IntParam(default=60)  # 4 bytes are added for CRC by NIC
-    frags = IntParam(default=1)
-    burst = IntParam(default=8)
+    src_port: IntParam = 9
+    dst_port: IntParam = 9  #  WARN: port 9 is discard protocol!
 
-    duration = IntParam(default=60)
+    count: IntParam = 0  # 0 = no upper limit
+    pkt_size: IntParam = 60  # 4 bytes are added for CRC by NIC
+    frags: IntParam = 1
+    burst: IntParam = 8
+
+    duration: IntParam = 60
+
+    ratep: IntParam = -1  # pps
+    flags: ListParam = field(default_factory=lambda: ["NO_TIMESTAMP", "QUEUE_MAP_CPU"])
+    vlan_id: IntParam = 0  # 0 is invalid vlan id, will be ignored
+
+    @staticmethod
+    def name_template(inf: Device, cpu: int) -> str:
+        return f"{inf.name}@{cpu}"
+
+    @property
+    def name(self):
+        return PktgenDevice.name_template(self.src_if, self.cpu)
+
+    def configure(self):
+        for flag in self.flags:
+            self._cmd(f"flag {flag}")
+
+        self._cmd(f"count {self.count}")
+        self._cmd(f"pkt_size {self.pkt_size}")
+
+        self._cmd(f"dst_mac {self.dst_mac}")
+        self._cmd(f"src_mac {self.src_if.hwaddr}")
+
+        if isinstance(self.src_ip, Ip4Address):
+            self._cmd(f"dst_min {self.dst_ip}")
+            self._cmd(f"dst_max {self.dst_ip}")
+            self._cmd(f"src_min {self.src_ip}")
+            self._cmd(f"src_max {self.src_ip}")
+        else:
+            self._cmd(f"dst6 {self.dst_ip}")
+            self._cmd(f"src6 {self.src_ip}")
+
+        self._cmd(f"udp_src_min {self.src_port}")
+        self._cmd(f"udp_src_max {self.src_port}")
+        self._cmd(f"udp_dst_min {self.dst_port}")
+        self._cmd(f"udp_dst_max {self.dst_port}")
+
+        if self.vlan_id > 0:
+            self._cmd(f"vlan_id {self.vlan_id}")
+
+        if self.ratep > 0:
+            self._cmd(f"ratep {self.ratep}")
+
+        self._cmd(f"burst {self.burst}")
+
+    def _cmd(self, cmd: str):
+        logging.debug(f"Writing {cmd} to {self.name}")
+        with open(f"/proc/net/pktgen/{self.name}", "w") as f:
+            f.write(f"{cmd}\n")
+
+
+@dataclass
+class PktgenThread:
+    """
+    Just a wrapper around pktgen thread. Each thread can have
+    multiple PktgenDevices generating packets.
+    """
+
+    cpu: int
+    devices: list[PktgenDevice] = field(init=False, default_factory=list)
+
+    def add_device(self, device: PktgenDevice):
+        logging.debug(f"Adding device {device.name} to cpu {self.cpu}")
+
+        self._cmd(f"add_device {device.name}")
+        self.devices.append(device)
+
+    def remove_all_devices(self):
+        logging.debug(f"Removing all devices from cpu{self.cpu}")
+
+        self._cmd("rem_device_all")
+
+        self.devices = []
+
+    def _cmd(self, cmd: str):
+        logging.debug(f"Writing {cmd} to cpu{self.cpu}")
+        with open(f"/proc/net/pktgen/kpktgend_{self.cpu}", "w") as f:
+            f.write(f"{cmd}\n")
+
+
+class PktgenController(BaseTestModule):
+    """
+    Think of this as a iperf client process, however pktgen
+    doesn't support multiple parallel processes. Therefore, single
+    PktGenController per networking namespace is allowed.
+
+    The config param represents a list of configs, each for individual
+    PktgenDevice (dicts are just passed to PktgenDevice), each cpu/thread
+    can be configured separately. This allows it to support running
+    multiple streams in parallel.
+
+    CPU pinning is handled by PktgenThread
+    Device config is handled by PktgenDevice
+
+
+    Args:
+        config (list): List of dicts, each representing a PktgenDevice
+            configuration. Each dict is passed directly to PktgenDevice.
+            E.g.: [{"cpu": 0, "src_if": ..., "dst_mac": ..., ...}]
+    """
+
+    config = ListParam()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self._devices = []
-
-        self._res_data = {}
-        self._output_parser = None
+        self._threads: list[PktgenThread] = []
 
     def run(self):
         self._load_pktgen_module()
-        self._pg_ctrl("reset")
-        self._configure_generator()
+        if not self._check_cpu_pinning():
+            logging.warning(
+                "Each device should be pinned to separate CPU."
+                "Results might not be stable otherwise."
+            )
 
-        output_parser = PktGenResultsSampler(self._devices, self.params.duration)
+        self._cmd("reset")
+        self._threads = self._setup_threads()
+
+        devices = [dev.name for thread in self._threads for dev in thread.devices]
+
+        output_parser = PktGenResultsSampler(devices, self.duration)
         output_parser.start_sampling()
 
         logging.debug("Starting generator")
         pktgen = Popen("echo 'start' > /proc/net/pktgen/pgctrl", shell=True)
         # ^^ echoing start to controller is blocking => needs to be separated
 
-        time.sleep(self.params.duration)
+        try:
+            time.sleep(self.duration)
+        except KeyboardInterrupt:
+            logging.info("Test interrupted, stopping")
 
         pktgen.kill()  # stops pktgen
 
         self._res_data = output_parser.device_samples
 
-        self._deconfigure_generator()
+        self._teardown()
         return True
+
+    def runtime_estimate(self):
+        return self.duration + 5
+
+    @property
+    def duration(self):
+        return max(thread["duration"] for thread in self.params.config)
+
+    def _setup_threads(self) -> list[PktgenThread]:
+        threads = []
+        for device in self.params.config:
+            thread = PktgenThread(device["cpu"])
+            dev = PktgenDevice(**device)
+            thread.add_device(dev)
+            dev.configure()
+
+            threads.append(thread)
+
+        return threads
+
+    def _teardown(self):
+        for thread in self._threads:
+            thread.remove_all_devices()
+
+        self._cmd("reset")
+
+    def _check_cpu_pinning(self):
+        # check if each device is pinned to separate CPU
+        return len(set(dev["cpu"] for dev in self.params.config)) == len(
+            self.params.config
+        )
 
     def _load_pktgen_module(self):
         try:
@@ -168,55 +328,6 @@ class PktGen(BaseTestModule):
         if not kmod_loaded("pktgen"):
             raise TestModuleError("pktgen module is not loaded")
 
-    def _configure_generator(self):
-        logging.debug("Configuring generator")
-
-        for cpu in self.params.cpus:
-            dev = f"{self.params.src_if.name}@{cpu}"
-            logging.debug(f"Adding interface {self.params.src_if.name} to cpu {cpu}")
-
-            self._pg_thread(cpu, f"add_device {dev}")
-
-            self._pg_set(cpu, "flag QUEUE_MAP_CPU")
-            self._pg_set(cpu, f"count {self.params.count}")
-            self._pg_set(cpu, f"pkt_size {self.params.pkt_size}")
-            self._pg_set(cpu, "flag NO_TIMESTAMP")
-
-            self._pg_set(cpu, f"dst_mac {self.params.dst_mac}")
-            self._pg_set(cpu, f"src_mac {self.params.src_if.hwaddr}")
-
-            if isinstance(self.params.src_ip, Ip4Address):
-                self._pg_set(cpu, f"dst_min {self.params.dst_ip}")
-                self._pg_set(cpu, f"dst_max {self.params.dst_ip}")
-                self._pg_set(cpu, f"src_min {self.params.src_ip}")
-                self._pg_set(cpu, f"src_max {self.params.src_ip}")
-            else:
-                self._pg_set(cpu, f"dst6 {self.params.dst_ip}")
-                self._pg_set(cpu, f"src6 {self.params.src_ip}")
-
-            self._pg_set(cpu, f"burst {self.params.burst}")
-            self._devices.append(dev)
-
-    def _deconfigure_generator(self):
-        logging.debug("Deconfiguring generator")
-        for cpu in self.params.cpus:
-            self._pg_thread(cpu, "rem_device_all")
-
-        self._pg_ctrl("reset")
-
-    def _pg_ctrl(self, cmd: str):
-        self._write_command("/proc/net/pktgen/pgctrl", cmd)
-
-    def _pg_thread(self, thread: int, cmd: str):
-        self._write_command(f"/proc/net/pktgen/kpktgend_{thread}", cmd)
-
-    def _pg_set(self, thread: int, cmd: str):
-        self._write_command(f"/proc/net/pktgen/{self.params.src_if.name}@{thread}", cmd)
-
-    def _write_command(self, file: str, cmd: str):
-        logging.debug(f"Writing {cmd} to {file}")
-        with open(file, "w") as f:
-            f.write(f"{cmd}\n")
-
-    def runtime_estimate(self):
-        return self.params.duration + 5
+    def _cmd(self, cmd):
+        with open("/proc/net/pktgen/pgctrl", "w") as f:
+            f.write(cmd + "\n")
