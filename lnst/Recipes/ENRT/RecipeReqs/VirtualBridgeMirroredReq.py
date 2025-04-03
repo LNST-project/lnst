6f51a7b7e5996ebf2dba0e70e2aa9b509e092c90
Jan Tluka <jtluka@redhat.com>
2025-04-03 16:22:00 +0200

    Recipes/ENRT/BaseEnrtRecipe.py: add nic_speed and nic_model params
    Signed-off-by: Jan Tluka <jtluka@redhat.com>

--- lnst/Recipes/ENRT/RecipeReqs/VirtualBridgeMirroredReq.py
+++ lnst/Recipes/ENRT/RecipeReqs/VirtualBridgeMirroredReq.py
@@ -1,13 +1,23 @@
 from lnst.Controller import HostReq, DeviceReq, RecipeParam
 
 
-class VirtualBridgeMirroredReq():
+class VirtualBridgeMirroredReq:
     host1 = HostReq()
-    host1.eth0 = DeviceReq(label="to_switch", driver=RecipeParam("driver"))
+    host1.eth0 = DeviceReq(
+        label="to_switch",
+        driver=RecipeParam("driver"),
+        speed=RecipeParam("nic_speed"),
+        model=RecipeParam("nic_model"),
+    )
     host1.tap0 = DeviceReq(label="to_guest1")
 
     host2 = HostReq()
-    host2.eth0 = DeviceReq(label="to_switch", driver=RecipeParam("driver"))
+    host2.eth0 = DeviceReq(
+        label="to_switch",
+        driver=RecipeParam("driver"),
+        speed=RecipeParam("nic_speed"),
+        model=RecipeParam("nic_model"),
+    )
     host2.tap0 = DeviceReq(label="to_guest2")
 
     guest1 = HostReq()
--- lnst/Recipes/ENRT/RecipeReqs/VirtualBridgeReq.py
+++ lnst/Recipes/ENRT/RecipeReqs/VirtualBridgeReq.py
@@ -1,13 +1,23 @@
 from lnst.Controller import HostReq, DeviceReq, RecipeParam
 
 
-class VirtualBridgeReq():
+class VirtualBridgeReq:
     host1 = HostReq()
-    host1.eth0 = DeviceReq(label="to_switch", driver=RecipeParam("driver"))
+    host1.eth0 = DeviceReq(
+        label="to_switch",
+        driver=RecipeParam("driver"),
+        speed=RecipeParam("nic_speed"),
+        model=RecipeParam("nic_model"),
+    )
     host1.tap0 = DeviceReq(label="to_guest")
 
     host2 = HostReq()
-    host2.eth0 = DeviceReq(label="to_switch", driver=RecipeParam("driver"))
+    host2.eth0 = DeviceReq(
+        label="to_switch",
+        driver=RecipeParam("driver"),
+        speed=RecipeParam("nic_speed"),
+        model=RecipeParam("nic_model"),
+    )
 
     guest1 = HostReq()
     guest1.eth0 = DeviceReq(label="to_guest")
