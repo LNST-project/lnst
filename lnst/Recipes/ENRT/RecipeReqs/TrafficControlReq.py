68dd583cd3583de91978d9745ea851870e192490
Jan Tluka <jtluka@redhat.com>
2025-04-04 11:41:11 +0200

    Recipes/TrafficControlRecipe.py: add nic_speed and nic_model parameters
    Signed-off-by: Jan Tluka <jtluka@redhat.com>

--- lnst/Recipes/ENRT/RecipeReqs/TrafficControlReq.py
+++ lnst/Recipes/ENRT/RecipeReqs/TrafficControlReq.py
@@ -3,4 +3,9 @@ from lnst.Controller import HostReq, DeviceReq, RecipeParam
 
 class TrafficControlReq:
     host1 = HostReq()
-    host1.eth0 = DeviceReq(label="net1", driver=RecipeParam("driver"))
+    host1.eth0 = DeviceReq(
+        label="net1",
+        driver=RecipeParam("driver"),
+        speed=RecipeParam("nic_speed"),
+        model=RecipeParam("nic_model"),
+    )
