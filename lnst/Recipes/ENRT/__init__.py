78a4c196982976deb688b8f669481012f957adca
Phil Sutter <psutter@redhat.com>
2025-06-18 15:06:31 +0200

    lnst: Recipes: Introduce SimpleNetnsRouterRecipe
    This is a derived class from SimpleNetworkRecipe which creates a netns
    on the second host, enables forwarding into it and returns it as second
    endpoint. Perf and ping tests will therefore excercise the second host's
    routing path.
    
    Signed-off-by: Phil Sutter <psutter@redhat.com>

--- lnst/Recipes/ENRT/__init__.py
+++ lnst/Recipes/ENRT/__init__.py
@@ -117,3 +117,4 @@ from lnst.Recipes.ENRT.CTInsertionRateNftablesRecipe import CTInsertionRateNftab
 from .CTFulltableInsertionRateRecipe import CTFulltableInsertionRateRecipe
 from lnst.Recipes.ENRT.BaseEnrtRecipe import BaseEnrtRecipe
 from lnst.Recipes.ENRT.BaseTunnelRecipe import BaseTunnelRecipe
+from lnst.Recipes.ENRT.SimpleNetnsRouterRecipe import SimpleNetnsRouterRecipe
