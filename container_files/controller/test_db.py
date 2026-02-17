6cc60beb3b46de0c688dfeee8a3f02e678efe639
Samuel Dobron <sdobron@redhat.com>
2026-02-17 20:50:22 +0100

    controller: added example recipes

--- container_files/controller/test_db.py
+++ container_files/controller/test_db.py
@@ -15,4 +15,48 @@ tests = [
     #         "driver": "ice",
     #     },
     # },
+    {
+        "recipe_name": "SimpleNetworkRecipe",
+        "params": {
+            "driver": "mlx5_core",
+            "perf_tool_cpu": [6],
+            "dev_intr_cpu": [0],
+            "perf_parallel_processes": 1,
+            "offload_combinations": [
+                {"gro": "on", "gso": "on", "tso": "on", "tx": "on", "rx": "on"}
+            ],
+            "perf_duration": 60,
+            "ip_versions": ["ipv4"],
+            "perf_tests": ["tcp_stream"],
+            "perf_msg_sizes": [131072],
+            "rx_pause_frames": False,
+            "tx_pause_frames": False,
+            "nic_speed": "100000",
+            "nic_model": "Mellanox-MT2910_Family",
+            "perf_iterations": 1,
+            "net_ipv4": "192.168.220.0/24",
+        },
+    },
+    {
+        "recipe_name": "SimpleNetworkRecipe",
+        "params": {
+            "driver": "mlx5_core",
+            "perf_tool_cpu": [6],
+            "dev_intr_cpu": [0],
+            "perf_parallel_processes": 1,
+            "offload_combinations": [
+                {"gro": "on", "gso": "on", "tso": "on", "tx": "on", "rx": "on"}
+            ],
+            "perf_duration": 60,
+            "ip_versions": ["ipv6"],
+            "perf_tests": ["tcp_stream"],
+            "perf_msg_sizes": [131072],
+            "rx_pause_frames": False,
+            "tx_pause_frames": False,
+            "nic_speed": "100000",
+            "nic_model": "Mellanox-MT2910_Family",
+            "perf_iterations": 1,
+            "net_ipv6": "fd00:0:b100::/64",
+        },
+    },
 ]
