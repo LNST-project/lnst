LNST-project/lnst

commit 13e17f19f9edf567bff1791b7bbde89cabc8cc17

lnst: Recipes: Customize chain spec in NftablesRuleScaleRecipe
Allow for users of the builtin firewall_rulesets_generator() to specify
the netfilter hook rules should be added to.

Use a generic name for the chain while at it.

lnst/Recipes/ENRT/NftablesRuleScaleRecipe.py
@@ -11,6 +11,10 @@ class NftablesRuleScaleRecipe(SimpleNetnsRouterRecipe, NftablesMixin):
     Rules are added to host2's forwarding hook which routes test traffic
     between host1 and a local netns.
 
+    :param chainspec:
+        The hook spec of the chain to add rules to.
+    :type chainspec: :any:`StrParam` (default "type filter hook forward priority filter")
+
     :param rule:
         The actual rule to insert repeatedly into the router's forwarding
         chain.
@@ -22,33 +26,41 @@ class NftablesRuleScaleRecipe(SimpleNetnsRouterRecipe, NftablesMixin):
 
     :param flowtable:
         Whether to offload established connections to a flowtable or not.
-    :type flowtable: :any:`BoolParam`
+        Besides creating a flowtable hooking into router's input and output
+        interfaces, this will append a final rule matching on conntrack state
+        'established' and adding the flow.
+    :type flowtable: :any:`BoolParam` (default False)
     """
+    chainspec = StrParam(mandatory=False,
+                         default="type filter hook forward priority filter")
     rule = StrParam(mandatory=True)
     scale = IntParam(mandatory=True)
     flowtable = BoolParam(mandatory=False, default=False)
 
     @property
     def firewall_rulesets_generator(self):
+        chainspec = self.params.get('chainspec')
         rule = self.params.get('rule')
         scale = self.params.get('scale')
         ruleset = [
             "flush ruleset",
             "add table inet t",
-            "add chain inet t forward { type filter hook forward priority filter; }",
-        ] + [f"add rule inet t forward {rule}"] * scale
+            f"add chain inet t c {{ {chainspec}; }}",
+        ] + [f"add rule inet t c {rule}"] * scale
         if self.params.get('flowtable'):
             devs = f"{self.matched.host2.eth0.name}, {self.matched.host2.pn0.name}"
             ftspec = f"hook ingress priority filter; devices = {{ {devs} }};"
             ruleset.append(f"add flowtable inet t ft {{ {ftspec} }}")
-            ruleset.append("add rule inet t forward ct state established flow add @ft")
+            ruleset.append("add rule inet t c ct state established flow add @ft")
         yield { self.matched.host2: "\n".join(ruleset) }
 
     def generate_sub_configuration_description(self, config):
+        chainspec = self.params.get('chainspec')
         rule = self.params.get('rule')
         scale = self.params.get('scale')
         desc = super().generate_sub_configuration_description(config)
         msg = f"NftablesScale: Ruleset with {scale} times '{rule}'"
+        msg += f" in a chain of '{chainspec}'"
         if self.params.get('flowtable'):
             msg += " and a flowtable"
         desc.append(msg)

commit b6a824e2342271ff17bf90caa7077070a45d4625

lnst: Recipes: Fix up NftablesRuleScaleRecipe documentation
The 'chainspec' extension missed to update docs accordingly.

Fixes: 13e17f19f9edf ("lnst: Recipes: Customize chain spec in NftablesRuleScaleRecipe")
Signed-off-by: Phil Sutter <psutter@redhat.com>

lnst/Recipes/ENRT/NftablesRuleScaleRecipe.py
@@ -8,16 +8,16 @@ class NftablesRuleScaleRecipe(SimpleNetnsRouterRecipe, NftablesMixin):
     routing throughput impact of specific nftables rules. To generate
     meaningful results, the rule is simply repeated multiple times, thereby
     amplifying the impact.
-    Rules are added to host2's forwarding hook which routes test traffic
-    between host1 and a local netns.
+    Rules are added to host2's forwarding hook by default (defined by
+    :any:`chainspec` parameter) which routes test traffic between host1 and a
+    local netns.
 
     :param chainspec:
         The hook spec of the chain to add rules to.
     :type chainspec: :any:`StrParam` (default "type filter hook forward priority filter")
 
     :param rule:
-        The actual rule to insert repeatedly into the router's forwarding
-        chain.
+        The actual rule to insert repeatedly into the router's chain.
     :type rule: :any:`StrParam` representing the nftables rule.
 
     :param scale: