6d5966e9eae680031c2076b616ce43ce1da32d19
Phil Sutter <psutter@redhat.com>
2026-07-24 11:52:58 +0200

    lnst: Recipes: Further extend NftablesRuleScaleRecipe parameters
    Allow users to customize the family and names of table and chain used
    for the sample ruleset.
    
    A later patch will add a parameter to specify custom nft commands so
    users may create e.g. a set or extra chain to refer to from the rule
    under test. Supporting format strings in there will reduce the needed
    guesswork and if they need to be present as variables one may just turn
    them into parameters themselves.
    
    Signed-off-by: Phil Sutter <psutter@redhat.com>

--- lnst/Recipes/ENRT/NftablesRuleScaleRecipe.py
+++ lnst/Recipes/ENRT/NftablesRuleScaleRecipe.py
@@ -12,6 +12,18 @@ class NftablesRuleScaleRecipe(SimpleNetnsRouterRecipe, NftablesMixin):
     :any:`chainspec` parameter) which routes test traffic between host1 and a
     local netns.
 
+    :param family:
+        The family to use for the tested ruleset.
+    :type family: :any:`StrParam` (default "inet")
+
+    :param tablename:
+        The name to use for the tested ruleset's table.
+    :type tablename: :any:`StrParam` (default "t")
+
+    :param chainname:
+        The name to use for the tested ruleset's chain.
+    :type chainname: :any:`StrParam` (default "c")
+
     :param chainspec:
         The hook spec of the chain to add rules to.
     :type chainspec: :any:`StrParam` (default "type filter hook forward priority filter")
@@ -31,6 +43,9 @@ class NftablesRuleScaleRecipe(SimpleNetnsRouterRecipe, NftablesMixin):
         'established' and adding the flow.
     :type flowtable: :any:`BoolParam` (default False)
     """
+    family = StrParam(mandatory=False, default="inet")
+    tablename = StrParam(mandatory=False, default="t")
+    chainname = StrParam(mandatory=False, default="c")
     chainspec = StrParam(mandatory=False,
                          default="type filter hook forward priority filter")
     rule = StrParam(mandatory=True)
@@ -39,28 +54,36 @@ class NftablesRuleScaleRecipe(SimpleNetnsRouterRecipe, NftablesMixin):
 
     @property
     def firewall_rulesets_generator(self):
+        family = self.params.get('family')
+        tablename = self.params.get('tablename')
+        chainname = self.params.get('chainname')
         chainspec = self.params.get('chainspec')
         rule = self.params.get('rule')
         scale = self.params.get('scale')
+        f_t_c = f"{family} {tablename} {chainname}"
         ruleset = [
             "flush ruleset",
-            "add table inet t",
-            f"add chain inet t c {{ {chainspec}; }}",
-        ] + [f"add rule inet t c {rule}"] * scale
+            f"add table {family} {tablename}",
+            f"add chain {f_t_c} {{ {chainspec}; }}"
+        ] + [f"add rule {f_t_c} {rule}"] * scale
         if self.params.get('flowtable'):
             devs = f"{self.matched.host2.eth0.name}, {self.matched.host2.pn0.name}"
             ftspec = f"hook ingress priority filter; devices = {{ {devs} }};"
-            ruleset.append(f"add flowtable inet t ft {{ {ftspec} }}")
-            ruleset.append("add rule inet t c ct state established flow add @ft")
+            ruleset.append(f"add flowtable {family} {tablename} ft {{ {ftspec} }}")
+            ruleset.append(f"add rule {f_t_c} ct state established flow add @ft")
         yield { self.matched.host2: "\n".join(ruleset) }
 
     def generate_sub_configuration_description(self, config):
+        family = self.params.get('family')
+        tablename = self.params.get('tablename')
+        chainname = self.params.get('chainname')
         chainspec = self.params.get('chainspec')
         rule = self.params.get('rule')
         scale = self.params.get('scale')
         desc = super().generate_sub_configuration_description(config)
         msg = f"NftablesScale: Ruleset with {scale} times '{rule}'"
-        msg += f" in a chain of '{chainspec}'"
+        msg += f" in a table named '{tablename}' and chain named '{chainname}'"
+        msg += f" with '{chainspec}' in family '{family}'"
         if self.params.get('flowtable'):
             msg += " and a flowtable"
         desc.append(msg)
